d7a33908989b2f2d25f5cf53090f49a7496c87f9
Jeffrey Morgan <jmorganca@gmail.com>
2021-01-22 20:15:08 -0500

    Add web server

--- cmd/server.go
+++ cmd/server.go
@@ -1,11 +1,14 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/infrahq/infra/server"
+	"github.com/spf13/cobra"
+)
 
 var serverCmd = &cobra.Command{
 	Use:   "server",
 	Short: "Run the infra server",
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Help()
+		server.Run()
 	},
 }
