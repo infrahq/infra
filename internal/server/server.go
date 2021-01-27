6f9e5d4c9dee4739de1bca01740228cc61efa809
Jeffrey Morgan <jmorganca@gmail.com>
2021-01-26 22:12:33 -0500

    Proxy all requests

--- internal/server/server.go
+++ internal/server/server.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"strings"
 
 	"github.com/julienschmidt/httprouter"
 
@@ -15,29 +14,34 @@ import (
 
 // Run runs the infra server
 func Run() {
-	// Create the proxy
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
 	}
 
-	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
-		Host:   strings.TrimPrefix(config.Host, "https://"),
-		Scheme: "https",
-	})
+	url, err := url.Parse(config.Host)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	transport, err := rest.TransportFor(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	proxy.Transport = transport
 
 	handler := func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		transport, _ := rest.TransportFor(config)
-		proxy.Transport = transport
 		proxy.ServeHTTP(w, r)
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		proxy.ServeHTTP(w, r)
-	})
-
 	router := httprouter.New()
 	router.GET("/*subpath", handler)
+	router.POST("/*subpath", handler)
+	router.PUT("/*subpath", handler)
+	router.PATCH("/*subpath", handler)
+	router.DELETE("/*subpath", handler)
 
 	fmt.Printf("Listening on port 3090")
 	log.Fatal(http.ListenAndServe(":3090", router))
