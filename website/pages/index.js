27e196ac5520dd80b07405fc98ea500a889b3131
Jeffrey Morgan <jmorgan@mba.local>
2021-03-11 13:40:18 -0500

    Add no-select

--- website/pages/index.js
+++ website/pages/index.js
@@ -8,7 +8,7 @@ export default function Home () {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </Head>
-      <main className='w-full h-full flex flex-col items-center justify-center'>
+      <main className='w-full h-full flex flex-col items-center justify-center select-none'>
         <img className='flex-none' src="/logo.svg" />
         <h2 className='text-gray-400 mt-6 font-medium'><a className='hover:underline' href="mailto:contact@infrahq.com">Contact Us</a></h2>
       </main>
