d62389e1659f704d2893591891bd13e98662601b
Jeffrey Morgan <jmorgan@mba.local>
2021-03-28 23:30:16 -0400

    New CLI

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/spf13/cobra v1.1.1
+	github.com/urfave/cli/v2 v2.3.0
 	gopkg.in/yaml.v2 v2.2.8
 	k8s.io/apimachinery v0.20.2 // indirect
 	k8s.io/client-go v0.20.0
--- main.go
+++ main.go
@@ -2,157 +2,85 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
-	"github.com/infrahq/infra/internal/server"
-	"github.com/spf13/cobra"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	var (
-		installCmd = &cobra.Command{
-			Use:   "install",
-			Short: "Install Infra",
-			Run: func(cmd *cobra.Command, args []string) {
-				fmt.Println("NOT IMPLEMENTED")
+	app := &cli.App{
+		Usage: "Manage infrastructure identity & access",
+		Commands: []*cli.Command{
+			{
+				Name:  "users",
+				Usage: "List users",
+				Action: func(c *cli.Context) error {
+					fmt.Println("NOT IMPLEMENTED", c.Args().First())
+					return nil
+				},
 			},
-		}
-	)
-
-	var (
-		serverPort int
-		serverCmd  = &cobra.Command{
-			Use:   "server",
-			Short: "Run the infra server",
-			Run: func(cmd *cobra.Command, args []string) {
-				config := &server.Options{
-					Port: serverPort,
-				}
-				if err := server.Run(config); err != nil {
-					panic(err)
-				}
+			{
+				Name:  "groups",
+				Usage: "List groups",
+				Action: func(c *cli.Context) error {
+					fmt.Println("NOT IMPLEMENTED", c.Args().First())
+					return nil
+				},
 			},
-		}
-	)
-	serverCmd.Flags().IntVarP(&serverPort, "port", "p", 3090, "Port to listen on")
-
-	var (
-		usersAddCmd = &cobra.Command{
-			Use:   "add",
-			Short: "Add a user",
-			Run: func(cmd *cobra.Command, args []string) {
-				fmt.Println("NOT IMPLEMENTED")
+			{
+				Name:  "roles",
+				Usage: "List available roles",
+				Action: func(c *cli.Context) error {
+					fmt.Println("NOT IMPLEMENTED", c.Args().First())
+					return nil
+				},
 			},
-		}
-	)
-
-	var (
-		usersRemoveCmd = &cobra.Command{
-			Use:     "remove",
-			Short:   "Remove a user",
-			Aliases: []string{"rm"},
-			Run: func(cmd *cobra.Command, args []string) {
-				fmt.Println("NOT IMPLEMENTED")
+			{
+				Name:  "permissions",
+				Usage: "List configured permissions",
+				Action: func(c *cli.Context) error {
+					fmt.Println("NOT IMPLEMENTED", c.Args().First())
+					return nil
+				},
 			},
-		}
-	)
-
-	var (
-		usersCmd = &cobra.Command{
-			Use:     "users",
-			Short:   "List & manage users",
-			Aliases: []string{"user"},
-			Run: func(cmd *cobra.Command, args []string) {
-				fmt.Println("NOT IMPLEMENTED")
+			{
+				Name:  "login",
+				Usage: "Login to an Infra registry",
+				Action: func(c *cli.Context) error {
+					fmt.Println("NOT IMPLEMENTED", c.Args().First())
+					return nil
+				},
 			},
-		}
-	)
-	usersCmd.AddCommand(usersAddCmd)
-	usersCmd.AddCommand(usersRemoveCmd)
-
-	var (
-		rolesAddCmd = &cobra.Command{
-			Use:   "add",
-			Short: "Add a role",
-			Run: func(cmd *cobra.Command, args []string) {
-				fmt.Println("NOT IMPLEMENTED")
+			{
+				Name:  "logout",
+				Usage: "Log out of an Infra registry",
+				Action: func(c *cli.Context) error {
+					fmt.Println("NOT IMPLEMENTED", c.Args().First())
+					return nil
+				},
 			},
-		}
-	)
-
-	var (
-		rolesRemoveCmd = &cobra.Command{
-			Use:     "remove",
-			Short:   "Remove a role",
-			Aliases: []string{"rm"},
-			Run: func(cmd *cobra.Command, args []string) {
-				fmt.Println("NOT IMPLEMENTED")
+			{
+				Name:  "install",
+				Usage: "Install Infra on a cluster",
+				Action: func(c *cli.Context) error {
+					fmt.Println("NOT IMPLEMENTED", c.Args().First())
+					return nil
+				},
 			},
-		}
-	)
-
-	var (
-		rolesCmd = &cobra.Command{
-			Use:     "roles",
-			Short:   "List & manage roles",
-			Aliases: []string{"role"},
-			Run: func(cmd *cobra.Command, args []string) {
-				fmt.Println("NOT IMPLEMENTED")
+			{
+				Name:  "start",
+				Usage: "Start the Infra Engine",
+				Action: func(c *cli.Context) error {
+					fmt.Println("NOT IMPLEMENTED", c.Args().First())
+					return nil
+				},
 			},
-		}
-	)
-	rolesCmd.AddCommand(rolesAddCmd)
-	rolesCmd.AddCommand(rolesRemoveCmd)
-
-	var (
-		resourcesAddCmd = &cobra.Command{
-			Use:   "add",
-			Short: "Add a resource",
-			Run: func(cmd *cobra.Command, args []string) {
-				fmt.Println("NOT IMPLEMENTED")
-			},
-		}
-	)
-
-	var (
-		resourcesRemoveCmd = &cobra.Command{
-			Use:     "remove",
-			Short:   "Remove a resource",
-			Aliases: []string{"rm"},
-			Run: func(cmd *cobra.Command, args []string) {
-				fmt.Println("NOT IMPLEMENTED")
-			},
-		}
-	)
-
-	var (
-		resourcesCmd = &cobra.Command{
-			Use:     "resources",
-			Short:   "List & manage resources",
-			Aliases: []string{"resource"},
-			Run: func(cmd *cobra.Command, args []string) {
-				fmt.Println("NOT IMPLEMENTED")
-			},
-		}
-	)
-	resourcesCmd.AddCommand(resourcesAddCmd)
-	resourcesCmd.AddCommand(resourcesRemoveCmd)
-
-	var rootCmd = &cobra.Command{
-		Use:   "infra",
-		Short: "Infra: user infrastructure",
-		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Help()
 		},
 	}
-	rootCmd.AddCommand(installCmd)
-	rootCmd.AddCommand(serverCmd)
-	rootCmd.AddCommand(usersCmd)
-	rootCmd.AddCommand(rolesCmd)
-	rootCmd.AddCommand(resourcesCmd)
 
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
