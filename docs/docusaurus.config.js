3fd08441410743a48a9bbd641d75f6ef57898ef5
Jeffrey Morgan <jmorganca@gmail.com>
2021-04-02 17:39:46 -0400

    Set base url to /docs/

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
