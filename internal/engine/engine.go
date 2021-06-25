199423ec53c31e0a025ef8d20f610002e6db31b4
jmorganca <251292+jmorganca@users.noreply.github.com>
2021-06-24 22:13:23 -0400

    Add engine jwt verification unit tests (#40)

--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -1,14 +1,12 @@
 package engine
 
 import (
-	"bytes"
 	"context"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httputil"
@@ -17,23 +15,11 @@ import (
 	"sync"
 	"time"
 
-	"github.com/gin-gonic/gin"
 	"github.com/goware/urlx"
 	"github.com/infrahq/infra/internal/registry"
 	"github.com/infrahq/infra/internal/timer"
-	"github.com/jessevdk/go-flags"
 	"gopkg.in/square/go-jose.v2"
 	"gopkg.in/square/go-jose.v2/jwt"
-	"gopkg.in/yaml.v2"
-	v1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
-	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/scheme"
-	rest "k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/tools/remotecommand"
 )
 
 type Options struct {
@@ -53,135 +39,31 @@ type RegistrationInfo struct {
 	ClusterEndpoint string
 }
 
-type Kubernetes struct {
-	mu     sync.Mutex
-	config *rest.Config
-}
-
-func NewKubernetes() (*Kubernetes, error) {
-	k := &Kubernetes{}
-
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		return k, err
-	}
-
-	k.config = config
-
-	return k, err
-}
-
-func (k *Kubernetes) UpdatePermissions(rbs []RoleBinding) error {
-	k.mu.Lock()
-	defer k.mu.Unlock()
-
-	subjects := make(map[string][]rbacv1.Subject)
-
-	for _, rb := range rbs {
-		subjects[rb.Role] = append(subjects[rb.Role], rbacv1.Subject{
-			APIGroup: "rbac.authorization.k8s.io",
-			Kind:     "User",
-			Name:     rb.User,
-		})
-	}
-
-	crbs := []*rbacv1.ClusterRoleBinding{}
-	for role, subjs := range subjects {
-		crbs = append(crbs, &rbacv1.ClusterRoleBinding{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "infra-" + role,
-				Labels: map[string]string{
-					"app.kubernetes.io/managed-by": "infra",
-				},
-			},
-			Subjects: subjs,
-			RoleRef: rbacv1.RoleRef{
-				APIGroup: "rbac.authorization.k8s.io",
-				Kind:     "ClusterRole",
-				Name:     role,
-			},
-		})
-	}
-
-	// TODO (jmorganca): find and delete empty rolebindings
-	// Create empty crbs for roles with no users
-	clientset, err := kubernetes.NewForConfig(k.config)
-	if err != nil {
-		return err
-	}
-
-	existingCrbs, err := clientset.RbacV1().ClusterRoleBindings().List(context.TODO(), metav1.ListOptions{LabelSelector: "app.kubernetes.io/managed-by=infra"})
-	if err != nil {
-		return err
-	}
-
-	// Create or update CRBs for users
-	for _, crb := range crbs {
-		_, err = clientset.RbacV1().ClusterRoleBindings().Update(context.TODO(), crb, metav1.UpdateOptions{})
-		if err != nil {
-			if k8sErrors.IsNotFound(err) {
-				_, err = clientset.RbacV1().ClusterRoleBindings().Create(context.TODO(), crb, metav1.CreateOptions{})
-				if err != nil {
-					return err
-				}
-			} else {
-				return err
-			}
-		}
-	}
-
-	// Delete any CRBs managed by infra that aren't in the config
-	var toDelete []rbacv1.ClusterRoleBinding
-	for _, e := range existingCrbs.Items {
-		var found bool
-		for _, crb := range crbs {
-			if crb.Name == e.Name {
-				found = true
-			}
-		}
-
-		if !found {
-			toDelete = append(toDelete, e)
-		}
-	}
-
-	for _, td := range toDelete {
-		err := clientset.RbacV1().ClusterRoleBindings().Delete(context.TODO(), td.Name, metav1.DeleteOptions{})
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
 type jwkCache struct {
 	mu          sync.Mutex
-	key         jose.JSONWebKey
+	key         *jose.JSONWebKey
 	lastChecked time.Time
+
+	client  *http.Client
+	baseURL string
 }
 
-var jwkCacheRefresh = 5 * time.Minute
-
-var cache jwkCache
-
-func (j *jwkCache) Get(client *http.Client, url string) (key jose.JSONWebKey, err error) {
+func (j *jwkCache) getjwk() (*jose.JSONWebKey, error) {
 	j.mu.Lock()
 	defer j.mu.Unlock()
 
-	if j.lastChecked != (time.Time{}) && time.Now().Before(j.lastChecked.Add(jwkCacheRefresh)) {
-		key = j.key
-		return
+	if j.lastChecked != (time.Time{}) && time.Now().Before(j.lastChecked.Add(JWKCacheRefresh)) {
+		return j.key, nil
 	}
 
-	res, err := client.Get(url)
+	res, err := j.client.Get(j.baseURL + "/.well-known/jwks.json")
 	if err != nil {
-		return
+		return nil, err
 	}
 
 	data, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return
+		return nil, err
 	}
 
 	var response struct {
@@ -189,48 +71,50 @@ func (j *jwkCache) Get(client *http.Client, url string) (key jose.JSONWebKey, er
 	}
 	err = json.Unmarshal(data, &response)
 	if err != nil {
-		return
+		return nil, err
 	}
 
 	if len(response.Keys) < 1 {
-		err = errors.New("no jwks provided by registry")
-		return
+		return nil, errors.New("no jwks provided by registry")
 	}
 
-	j.key = response.Keys[0]
 	j.lastChecked = time.Now()
+	j.key = &response.Keys[0]
 
-	return j.key, nil
+	return &response.Keys[0], nil
 }
 
-func JWTMiddleware(client *http.Client, base string) func(c *gin.Context) {
-	return func(c *gin.Context) {
-		// Check bearer header
-		authorization := c.Request.Header.Get("X-Infra-Authorization")
+var JWKCacheRefresh = 5 * time.Minute
+
+type GetJWKFunc func() (*jose.JSONWebKey, error)
+
+type HttpContextKeyEmail struct{}
+
+func jwtMiddleware(getjwk GetJWKFunc, next http.HandlerFunc) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authorization := r.Header.Get("X-Infra-Authorization")
 		raw := strings.Replace(authorization, "Bearer ", "", -1)
 		if raw == "" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
 			return
 		}
 
 		tok, err := jwt.ParseSigned(raw)
 		if err != nil {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
 			return
 		}
 
-		key, err := cache.Get(client, base+"/.well-known/jwks.json")
+		key, err := getjwk()
 		if err != nil {
-			fmt.Println(err)
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
 			return
 		}
 
 		out := make(map[string]interface{})
 		cl := jwt.Claims{}
 		if err := tok.Claims(key, &cl, &out); err != nil {
-			fmt.Println("could not verify token", err)
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
 			return
 		}
 
@@ -240,35 +124,31 @@ func JWTMiddleware(client *http.Client, base string) func(c *gin.Context) {
 		})
 		switch {
 		case errors.Is(err, jwt.ErrExpired):
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "expired"})
+			http.Error(w, "expired", http.StatusUnauthorized)
 			return
 		case err != nil:
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
 			return
 		}
 
-		tok.UnsafeClaimsWithoutVerification(&out)
+		email, ok := out["email"].(string)
+		if !ok {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
 
-		email := out["email"].(string)
-
-		c.Set("email", email)
-		c.Next()
-	}
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, HttpContextKeyEmail{}, email)
+		next(w, r.WithContext(ctx))
+	})
 }
 
-func (k *Kubernetes) ProxyHandler() (handler gin.HandlerFunc, err error) {
-	remote, err := url.Parse(k.config.Host)
-	if err != nil {
-		return
-	}
-
-	ca, err := ioutil.ReadFile(k.config.TLSClientConfig.CAFile)
-	if err != nil {
-		return
-	}
-
+func proxyHandler(ca []byte, bearerToken string, remote *url.URL) (http.HandlerFunc, error) {
 	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(ca)
+	ok := caCertPool.AppendCertsFromPEM(ca)
+	if !ok {
+		return nil, errors.New("could not append ca to client cert bundle")
+	}
 	proxy := httputil.NewSingleHostReverseProxy(remote)
 	proxy.Transport = &http.Transport{
 		TLSClientConfig: &tls.Config{
@@ -276,16 +156,22 @@ func (k *Kubernetes) ProxyHandler() (handler gin.HandlerFunc, err error) {
 		},
 	}
 
-	return func(c *gin.Context) {
-		email := c.GetString("email")
-		c.Request.Header.Del("X-Infra-Authorization")
-		c.Request.Header.Set("Impersonate-User", email)
-		c.Request.Header.Add("Authorization", "Bearer "+string(k.config.BearerToken))
-		http.StripPrefix("/proxy", proxy).ServeHTTP(c.Writer, c.Request)
-	}, err
+	return func(w http.ResponseWriter, r *http.Request) {
+		email, ok := r.Context().Value(HttpContextKeyEmail{}).(string)
+		if !ok {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		r.Header.Del("X-Infra-Authorization")
+		r.Header.Set("Impersonate-User", email)
+		r.Header.Add("Authorization", "Bearer "+bearerToken)
+
+		http.StripPrefix("/proxy", proxy).ServeHTTP(w, r)
+	}, nil
 }
 
-func fetchConfig(client *http.Client, base string, name string) ([]registry.Permission, error) {
+func fetchPermissions(client *http.Client, base string, name string) ([]registry.Permission, error) {
 	params := url.Values{}
 	params.Add("name", name)
 
@@ -320,166 +206,6 @@ func fetchConfig(client *http.Client, base string, name string) ([]registry.Perm
 	}
 
 	return response.Data, nil
-}
-
-func (k *Kubernetes) CA() ([]byte, error) {
-	contents, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
-	if err != nil {
-		return nil, err
-	}
-	return contents, nil
-}
-
-func (k *Kubernetes) ExecCat(pod string, namespace string, file string) (string, error) {
-	clientset, err := kubernetes.NewForConfig(k.config)
-	if err != nil {
-		return "", err
-	}
-
-	cmd := []string{
-		"/bin/cat",
-		file,
-	}
-	req := clientset.CoreV1().RESTClient().Post().Resource("pods").Name(pod).Namespace(namespace).SubResource("exec")
-	req.VersionedParams(
-		&v1.PodExecOptions{
-			Command: cmd,
-			Stdout:  true,
-		},
-		scheme.ParameterCodec,
-	)
-
-	exec, err := remotecommand.NewSPDYExecutor(k.config, "POST", req.URL())
-	if err != nil {
-		return "", err
-	}
-
-	var buf bytes.Buffer
-	err = exec.Stream(remotecommand.StreamOptions{
-		Stdout: io.Writer(&buf),
-	})
-	if err != nil {
-		return "", err
-	}
-
-	return buf.String(), nil
-}
-
-// Endpoint gets the cluster endpoint from within the pod
-func (k *Kubernetes) Endpoint() (string, error) {
-	// TODO (jmorganca): find and delete empty rolebindings
-	// Create empty crbs for roles with no users
-	clientset, err := kubernetes.NewForConfig(k.config)
-	if err != nil {
-		return "", err
-	}
-
-	var endpoint string
-
-	// Get the full command line for kube-proxy pods
-	// if --master is specified, use that
-	// if --kubeconfig is specified, exec + cat to read that
-	// if --config is specified, exec + cat the file the kubeconfig location, and read the kubeconfig
-
-	pods1, err := clientset.CoreV1().Pods("kube-system").List(context.TODO(), metav1.ListOptions{
-		LabelSelector: "k8s-app=kube-proxy",
-	})
-	if err != nil {
-		return "", err
-	}
-
-	pods2, err := clientset.CoreV1().Pods("kube-system").List(context.TODO(), metav1.ListOptions{
-		LabelSelector: "component=kube-proxy",
-	})
-	if err != nil {
-		return "", err
-	}
-
-	pods := append(pods1.Items, pods2.Items...)
-
-	if len(pods) == 0 {
-		return "", errors.New("no kube-proxy pods to inspect")
-	}
-
-	pod := pods[0]
-
-	var command []string
-	for _, c := range pod.Spec.Containers {
-		if c.Name == "kube-proxy" {
-			command = c.Command
-			break
-		}
-	}
-
-	var opts struct {
-		Master     string `long:"master"`
-		Config     string `long:"config"`
-		Kubeconfig string `long:"kubeconfig"`
-	}
-
-	p := flags.NewParser(&opts, flags.IgnoreUnknown)
-	_, err = p.ParseArgs(command[1:])
-	if err != nil {
-		return "", err
-	}
-
-	switch {
-	case opts.Master != "":
-		endpoint = opts.Master
-	case opts.Config != "":
-		contents, err := k.ExecCat(pod.Name, "kube-system", opts.Config)
-		if err != nil {
-			return "", err
-		}
-		var raw map[string]interface{}
-		err = yaml.Unmarshal([]byte(contents), &raw)
-		if err != nil {
-			return "", err
-		}
-
-		clientConnection, ok := raw["clientConnection"].(map[interface{}]interface{})
-		if !ok {
-			return "", errors.New("invalid kube-proxy config format")
-		}
-		kubeconfig, ok := clientConnection["kubeconfig"].(string)
-		if !ok {
-			return "", errors.New("invalid kube-proxy config format")
-		}
-
-		opts.Kubeconfig = kubeconfig
-		fallthrough
-	case opts.Kubeconfig != "":
-		contents, err := k.ExecCat(pod.Name, "kube-system", opts.Kubeconfig)
-		if err != nil {
-			return "", err
-		}
-
-		cfg, err := clientcmd.NewClientConfigFromBytes([]byte(contents))
-		if err != nil {
-			return "", err
-		}
-
-		rc, err := cfg.ClientConfig()
-		if err != nil {
-			return "", err
-		}
-
-		endpoint = rc.Host
-	}
-
-	// Rewrite docker desktop
-	if endpoint == "https://vm.docker.internal:6443" {
-		endpoint = "https://kubernetes.docker.internal:6443"
-	}
-
-	if strings.HasSuffix(endpoint, ".internal.k8s.ondigitalocean.com") {
-		endpoint = strings.Replace(endpoint, ".internal.k8s.ondigitalocean.com", ".k8s.ondigitalocean.com", -1)
-	}
-
-	// TODO (jmorganca): minikube
-
-	// Could not get endpoint - must be passed via flag
-	return endpoint, nil
 }
 
 type BearerTransport struct {
@@ -520,8 +246,6 @@ func Run(options Options) error {
 
 	timer := timer.Timer{}
 	timer.Start(5, func() {
-		fmt.Println("sync start")
-
 		ca, err := kubernetes.CA()
 		if err != nil {
 			fmt.Println(err)
@@ -546,7 +270,7 @@ func Run(options Options) error {
 		}
 
 		// Fetch latest permissions from server
-		permissions, err := fetchConfig(client, uri.String(), options.Name)
+		permissions, err := fetchPermissions(client, uri.String(), options.Name)
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -565,24 +289,34 @@ func Run(options Options) error {
 	})
 	defer timer.Stop()
 
-	gin.SetMode(gin.ReleaseMode)
-	router := gin.New()
-	router.Use(gin.Logger())
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
-	proxyHandler, err := kubernetes.ProxyHandler()
+	remote, err := url.Parse(kubernetes.config.Host)
 	if err != nil {
 		return err
 	}
 
-	router.GET("/healthz", func(c *gin.Context) {
-		c.Status(http.StatusOK)
-	})
-	router.GET("/proxy/*all", JWTMiddleware(client, uri.String()), proxyHandler)
-	router.POST("/proxy/*all", JWTMiddleware(client, uri.String()), proxyHandler)
-	router.PUT("/proxy/*all", JWTMiddleware(client, uri.String()), proxyHandler)
-	router.PATCH("/proxy/*all", JWTMiddleware(client, uri.String()), proxyHandler)
-	router.DELETE("/proxy/*all", JWTMiddleware(client, uri.String()), proxyHandler)
+	ca, err := ioutil.ReadFile(kubernetes.config.TLSClientConfig.CAFile)
+	if err != nil {
+		return err
+	}
+
+	ph, err := proxyHandler(ca, kubernetes.config.BearerToken, remote)
+	if err != nil {
+		return err
+	}
+
+	cache := jwkCache{
+		client:  client,
+		baseURL: uri.String(),
+	}
+
+	mux.Handle("/proxy/", jwtMiddleware(cache.getjwk, ph))
 
 	fmt.Println("serving on port 80")
-	return router.Run(":80")
+	return http.ListenAndServe(":80", mux)
 }
---
+++ internal/engine/engine_test.go
@@ -0,0 +1,320 @@
+package engine
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/base64"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/square/go-jose.v2"
+	"gopkg.in/square/go-jose.v2/jwt"
+)
+
+func TestJWTMiddlewareNoAuthHeader(t *testing.T) {
+	getJwkFunc := func() (*jose.JSONWebKey, error) {
+		return &jose.JSONWebKey{}, nil
+	}
+
+	emptyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	rr := httptest.NewRecorder()
+
+	handler := jwtMiddleware(getJwkFunc, emptyHandler)
+
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusUnauthorized, rr.Result().StatusCode)
+}
+
+func TestJWTMiddlewareNoToken(t *testing.T) {
+	getJwkFunc := func() (*jose.JSONWebKey, error) {
+		return &jose.JSONWebKey{}, nil
+	}
+
+	emptyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	rr := httptest.NewRecorder()
+
+	handler := jwtMiddleware(getJwkFunc, emptyHandler)
+
+	req, err := http.NewRequest("GET", "/", nil)
+	req.Header.Set("X-Infra-Authorization", "username:password")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusUnauthorized, rr.Result().StatusCode)
+}
+
+func TestJWTMiddlewareInvalidJWKs(t *testing.T) {
+	getJwkFunc := func() (*jose.JSONWebKey, error) {
+		return nil, errors.New("could not fetch JWKs")
+	}
+
+	emptyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	rr := httptest.NewRecorder()
+
+	handler := jwtMiddleware(getJwkFunc, emptyHandler)
+
+	req, err := http.NewRequest("GET", "/", nil)
+	req.Header.Set("X-Infra-Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJ0ZXN0QHRlc3QuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.j7o5o8GBkybaYXdFJIi8O6mPF50E-gJWZ3reLfMQD68")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusUnauthorized, rr.Result().StatusCode)
+}
+
+func generateJWK() (pub *jose.JSONWebKey, priv *jose.JSONWebKey, err error) {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	priv = &jose.JSONWebKey{Key: key, KeyID: "", Algorithm: string(jose.RS256), Use: "sig"}
+	thumb, err := priv.Thumbprint(crypto.SHA256)
+	if err != nil {
+		return nil, nil, err
+	}
+	kid := base64.URLEncoding.EncodeToString(thumb)
+	priv.KeyID = kid
+	pub = &jose.JSONWebKey{Key: &key.PublicKey, KeyID: kid, Algorithm: string(jose.RS256), Use: "sig"}
+
+	return pub, priv, err
+}
+
+func generateJWT(priv *jose.JSONWebKey, expiry time.Time) (string, error) {
+	signer, err := jose.NewSigner(jose.SigningKey{Algorithm: jose.RS256, Key: priv}, (&jose.SignerOptions{}).WithType("JWT"))
+	if err != nil {
+		return "", err
+	}
+
+	cl := jwt.Claims{
+		Issuer:   "infra",
+		Expiry:   jwt.NewNumericDate(expiry),
+		IssuedAt: jwt.NewNumericDate(time.Now()),
+	}
+	custom := struct {
+		Email string `json:"email"`
+		Nonce string `json:"nonce"`
+	}{
+		"test@test.com",
+		"randomstring",
+	}
+
+	raw, err := jwt.Signed(signer).Claims(cl).Claims(custom).CompactSerialize()
+	if err != nil {
+		return "", err
+	}
+
+	return raw, nil
+}
+
+func TestJWTMiddlewareInvalidJWT(t *testing.T) {
+	pub, _, err := generateJWK()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	invalidjwt := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJ0ZXN0QHRlc3QuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.j7o5o8GBkybaYXdFJIi8O6mPF50E-gJWZ3reLfMQD68"
+
+	getJwkFunc := func() (*jose.JSONWebKey, error) {
+		return pub, nil
+	}
+
+	emptyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	rr := httptest.NewRecorder()
+
+	handler := jwtMiddleware(getJwkFunc, emptyHandler)
+
+	req, err := http.NewRequest("GET", "/", nil)
+	req.Header.Set("X-Infra-Authorization", "Bearer "+invalidjwt)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusUnauthorized, rr.Result().StatusCode)
+}
+
+func TestJWTMiddlewareExpiredJWT(t *testing.T) {
+	pub, priv, err := generateJWK()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expiredJWT, err := generateJWT(priv, time.Now().Add(-1*time.Hour))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	getJwkFunc := func() (*jose.JSONWebKey, error) {
+		return pub, nil
+	}
+
+	emptyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	rr := httptest.NewRecorder()
+
+	handler := jwtMiddleware(getJwkFunc, emptyHandler)
+
+	req, err := http.NewRequest("GET", "/", nil)
+	req.Header.Set("X-Infra-Authorization", "Bearer "+expiredJWT)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusUnauthorized, rr.Result().StatusCode)
+
+	data, err := ioutil.ReadAll(rr.Result().Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "expired\n", string(data))
+}
+
+func TestJWTMiddlewareWrongHeader(t *testing.T) {
+	pub, priv, err := generateJWK()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expiredJWT, err := generateJWT(priv, time.Now().Add(-1*time.Hour))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	getJwkFunc := func() (*jose.JSONWebKey, error) {
+		return pub, nil
+	}
+
+	emptyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	rr := httptest.NewRecorder()
+
+	handler := jwtMiddleware(getJwkFunc, emptyHandler)
+
+	req, err := http.NewRequest("GET", "/", nil)
+	req.Header.Set("Authorization", "Bearer "+expiredJWT)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusUnauthorized, rr.Result().StatusCode)
+}
+
+func TestJWTMiddlewareValidJWTSetsEmail(t *testing.T) {
+	pub, priv, err := generateJWK()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	validJWT, err := generateJWT(priv, time.Now().Add(3*time.Hour))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	getJwkFunc := func() (*jose.JSONWebKey, error) {
+		return pub, nil
+	}
+
+	emptyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		email, ok := r.Context().Value(HttpContextKeyEmail{}).(string)
+		if !ok {
+			t.Fatal("could not parse email")
+		}
+		assert.Equal(t, "test@test.com", email)
+
+		w.WriteHeader(http.StatusOK)
+	})
+
+	rr := httptest.NewRecorder()
+
+	handler := jwtMiddleware(getJwkFunc, emptyHandler)
+
+	req, err := http.NewRequest("GET", "/", nil)
+	req.Header.Set("X-Infra-Authorization", "Bearer "+validJWT)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Result().StatusCode)
+}
+
+func TestProxyHandler(t *testing.T) {
+	pub, priv, err := generateJWK()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	validJWT, err := generateJWT(priv, time.Now().Add(3*time.Hour))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	getJwkFunc := func() (*jose.JSONWebKey, error) {
+		return pub, nil
+	}
+
+	emptyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		email, ok := r.Context().Value(HttpContextKeyEmail{}).(string)
+		if !ok {
+			t.Fatal("could not parse email")
+		}
+		assert.Equal(t, "test@test.com", email)
+
+		w.WriteHeader(http.StatusOK)
+	})
+
+	rr := httptest.NewRecorder()
+
+	handler := jwtMiddleware(getJwkFunc, emptyHandler)
+
+	req, err := http.NewRequest("GET", "/", nil)
+	req.Header.Set("X-Infra-Authorization", "Bearer "+validJWT)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Result().StatusCode)
+}
