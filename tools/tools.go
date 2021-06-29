1dddcc18400e0ab377ec53646ecb27769da8869e
jmorganca <jmorganca@gmail.com>
2021-06-28 22:09:47 -0400

    Add missing generate commands for tools.go

--- tools/tools.go
+++ tools/tools.go
@@ -2,6 +2,9 @@
 
 package tools
 
+//go:generate go install github.com/kevinburke/go-bindata
+//go:generate go install github.com/mitchellh/gon/cmd/gon
+
 import (
 	_ "github.com/kevinburke/go-bindata"
 	_ "github.com/mitchellh/gon/cmd/gon"
