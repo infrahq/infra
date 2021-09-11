b41c5bc8d76fa099d5c59203d1c4d2ae8e002198
jmorganca <jmorganca@gmail.com>
2021-09-11 19:54:01 -0400

    remove default view role

--- internal/registry/api_test.go
+++ internal/registry/api_test.go
@@ -446,12 +446,6 @@ func TestListRolesForClusterReturnsRolesFromConfig(t *testing.T) {
 		returnedUserRoles[r.Name] = r.Users
 	}
 
-	// check default roles granted on user create
-	assert.Equal(t, 3, len(returnedUserRoles["view"]))
-	assert.True(t, containsUser(returnedUserRoles["view"], iosDevUser.Email))
-	assert.True(t, containsUser(returnedUserRoles["view"], standardUser.Email))
-	assert.True(t, containsUser(returnedUserRoles["view"], adminUser.Email))
-
 	// roles from groups
 	assert.Equal(t, 2, len(returnedUserRoles["writer"]))
 	assert.True(t, containsUser(returnedUserRoles["writer"], iosDevUser.Email))
