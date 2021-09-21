3ec5d77c8f7833b8f06bdae2a5c31274b4c26e27
Steven Soroka <steven@infrahq.com>
2021-09-21 12:57:32 -0500

    cleanup client proxy

--- internal/cmd/client.go
+++ internal/cmd/client.go
@@ -29,6 +29,7 @@ const (
 var (
 	ClientTimeoutDuration = 5 * time.Minute
 	errorLogger           log.Logger // writes errors from the proxy handler to a file
+	timer                 *time.Timer
 )
 
 func RunLocalClient() error {
@@ -43,93 +44,14 @@ func RunLocalClient() error {
 	})
 	errorLogger.SetPrefix(time.Now().Format("2006-01-02 15:04:05 "))
 
-	timer := time.NewTimer(ClientTimeoutDuration)
+	timer = time.NewTimer(ClientTimeoutDuration)
 	go func() {
 		<-timer.C
-		os.Exit(0)
+		os.Exit(0) // todo: graceful shutdown
 	}()
 
-	proxyHandler := func(w http.ResponseWriter, r *http.Request) {
-		components := strings.Split(r.URL.Path, "/")
-		if len(components) < 3 {
-			http.Error(w, "path not found", http.StatusNotFound)
-			errorLogger.Println("invalid proxy URL path specified: " + r.URL.Path)
-			return
-		}
-
-		name := components[2]
-
-		contents, err := ioutil.ReadFile(filepath.Join(homeDir, INFRA_HIDDEN_DIR, "destinations"))
-		if err != nil {
-			fmt.Println(err)
-			errorLogger.Println(err)
-			return
-		}
-
-		var destinations []api.Destination
-		err = json.Unmarshal(contents, &destinations)
-		if err != nil {
-			http.Error(w, "could not read destinations from ~/.infra/destinations", http.StatusInternalServerError)
-			fmt.Println(err)
-			errorLogger.Println(err)
-			return
-		}
-
-		var destination *api.Destination
-		for i := range destinations {
-			if destinations[i].Name == name {
-				destination = &destinations[i]
-			}
-		}
-
-		if destination == nil {
-			errMsg := "could not load destination information for destination " + name
-			fmt.Println(errMsg)
-			errorLogger.Println(err)
-			return
-		}
-
-		var endpoint, ca, saToken string
-		namespace := "default"
-
-		if kube := destination.Kubernetes; kube != nil {
-			endpoint = kube.Endpoint
-			ca = kube.Ca
-			namespace = kube.Namespace
-			saToken = kube.SaToken
-		}
-
-		remote, err := urlx.Parse(endpoint + "/api/v1/namespaces/" + namespace + "/services/https:infra-engine:https/proxy/proxy")
-		if err != nil {
-			errorLogger.Println(err)
-			return
-		}
-
-		remote.Scheme = "https"
-
-		caCertPool := x509.NewCertPool()
-		caCertPool.AppendCertsFromPEM([]byte(ca))
-		proxy := httputil.NewSingleHostReverseProxy(remote)
-		proxy.Transport = &http.Transport{
-			TLSClientConfig: &tls.Config{
-				RootCAs: caCertPool,
-			},
-		}
-
-		timer.Reset(ClientTimeoutDuration)
-
-		if r.Header.Get("Upgrade") != "" {
-			r.Header.Add("X-Infra-Query", r.URL.RawQuery)
-		}
-
-		r.Header.Add("X-Infra-Authorization", r.Header.Get("Authorization"))
-		r.Header.Set("Authorization", "Bearer "+saToken)
-
-		http.StripPrefix("/client/"+name, proxy).ServeHTTP(w, r)
-	}
-
 	mux := http.NewServeMux()
-	mux.HandleFunc("/client/", proxyHandler)
+	mux.HandleFunc("/client/", clientProxyHandler)
 
 	certBytes, err := ioutil.ReadFile(filepath.Join(homeDir, INFRA_HIDDEN_DIR, CLIENT_DIR, "cert.pem"))
 	if err != nil {
@@ -169,4 +91,85 @@ func RunLocalClient() error {
 	}
 
 	return tlsServer.ServeTLS(l, "", "")
+}
+
+func clientProxyHandler(w http.ResponseWriter, r *http.Request) {
+	homeDir, _ := os.UserHomeDir() // home dir already error checked
+
+	components := strings.Split(r.URL.Path, "/")
+	if len(components) < 3 {
+		http.Error(w, "path not found", http.StatusNotFound)
+		errorLogger.Println("invalid proxy URL path specified: " + r.URL.Path)
+		return
+	}
+
+	name := components[2]
+
+	contents, err := ioutil.ReadFile(filepath.Join(homeDir, INFRA_HIDDEN_DIR, "destinations"))
+	if err != nil {
+		fmt.Println(err)
+		errorLogger.Println(err)
+		return
+	}
+
+	var destinations []api.Destination
+	err = json.Unmarshal(contents, &destinations)
+	if err != nil {
+		http.Error(w, "could not read destinations from ~/.infra/destinations", http.StatusInternalServerError)
+		fmt.Println(err)
+		errorLogger.Println(err)
+		return
+	}
+
+	var destination *api.Destination
+	for i := range destinations {
+		if destinations[i].Name == name {
+			destination = &destinations[i]
+		}
+	}
+
+	if destination == nil {
+		errMsg := "could not load destination information for destination " + name
+		fmt.Println(errMsg)
+		errorLogger.Println(err)
+		return
+	}
+
+	var endpoint, ca, saToken string
+	namespace := "default"
+
+	if kube := destination.Kubernetes; kube != nil {
+		endpoint = kube.Endpoint
+		ca = kube.Ca
+		namespace = kube.Namespace
+		saToken = kube.SaToken
+	}
+
+	remote, err := urlx.Parse(endpoint + "/api/v1/namespaces/" + namespace + "/services/https:infra-engine:https/proxy/proxy")
+	if err != nil {
+		errorLogger.Println(err)
+		return
+	}
+
+	remote.Scheme = "https"
+
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM([]byte(ca))
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	proxy.Transport = &http.Transport{
+		TLSClientConfig: &tls.Config{
+			RootCAs: caCertPool,
+		},
+	}
+
+	timer.Reset(ClientTimeoutDuration)
+
+	if r.Header.Get("Upgrade") != "" {
+		r.Header.Add("X-Infra-Query", r.URL.RawQuery)
+	}
+
+	r.Header.Add("X-Infra-Authorization", r.Header.Get("Authorization"))
+	r.Header.Set("Authorization", "Bearer "+saToken)
+
+	http.StripPrefix("/client/"+name, proxy).ServeHTTP(w, r)
 }
