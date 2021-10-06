ef270a14f93b488a30a065492b668260e88c568d
Michael Yang <michael.yang@infrahq.com>
2021-10-06 14:40:53 -0700

    warn user if either the client or server is out of date (#401)
    * warn if client version behind server
    
    * get update info from upstream
    
    * publish version as "latest"

--- internal/cmd/version.go
+++ internal/cmd/version.go
@@ -3,33 +3,90 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
 	"text/tabwriter"
 
 	"github.com/infrahq/infra/internal"
+	"golang.org/x/mod/semver"
 )
 
-func version() error {
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight)
-	defer w.Flush()
-	fmt.Fprintln(w)
-	fmt.Fprintln(w, "Client:\t", internal.Version)
+type VersionOptions struct {
+	Client   bool
+	Registry bool
+}
 
-	client, err := apiClientFromConfig()
-	if err != nil {
-		fmt.Fprintln(w, blue("✕")+" Could not retrieve client version")
-		return err
-	}
+func version(options VersionOptions) error {
+	clientSemVer := fmt.Sprintf("v%s", internal.Version)
+	serverVersion := "not connected"
 
 	// Note that we use the client to get this version, but it is in fact the server version
-	res, _, err := client.VersionApi.Version(context.Background()).Execute()
+	client, err := apiClientFromConfig()
+	if err == nil {
+		res, _, err := client.VersionApi.Version(context.Background()).Execute()
+		if err == nil {
+			serverVersion = res.Version
+		}
+	}
+
+	serverSemVer := fmt.Sprintf("v%s", serverVersion)
+
+	err = checkUpdate(clientSemVer, serverSemVer)
 	if err != nil {
-		fmt.Fprintln(w, "Registry:\t", "not connected")
+		fmt.Fprintln(os.Stderr, "Failed checking for updates:", err.Error())
+	}
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight)
+	defer w.Flush()
+
+	fmt.Fprintln(w)
+
+	if !options.Registry {
+		fmt.Fprintln(w, "Client:\t", clientSemVer)
+	}
+
+	if !options.Client {
+		fmt.Fprintln(w, "Registry:\t", serverSemVer)
+	}
+
+	fmt.Fprintln(w)
+
+	return nil
+}
+
+func checkUpdate(clientSemVer, serverSemVer string) error {
+	latestSemVer := "nonexistent"
+
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, "https://releases.infrahq.com/infra/latest", nil)
+	if err != nil {
 		return err
 	}
 
-	fmt.Fprintln(w, "Registry:\t", res.Version)
-	fmt.Fprintln(w)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode >= 400 {
+		return fmt.Errorf("%s", res.Status)
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err == nil {
+		latestSemVer = strings.TrimSpace(string(body))
+	}
+
+	if semver.Compare(latestSemVer, clientSemVer) > 0 {
+		fmt.Fprintf(os.Stderr, "Your client (%s) is out of date. Please update to %s.\n", clientSemVer, latestSemVer)
+	}
+
+	if semver.IsValid(serverSemVer) && semver.Compare(latestSemVer, serverSemVer) > 0 {
+		fmt.Fprintf(os.Stderr, "Your server (%s) is out of date. Please update to %s.\n", serverSemVer, latestSemVer)
+	}
 
 	return nil
 }
