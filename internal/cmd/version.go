f30abc2beefb5efd577cf7d909a15ce0dd94e260
Michael Yang <michael.yang@infrahq.com>
2021-10-05 19:20:10 -0700

    get update info from upstream

--- internal/cmd/version.go
+++ internal/cmd/version.go
@@ -3,7 +3,10 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
 	"text/tabwriter"
 
 	"github.com/infrahq/infra/internal"
@@ -16,10 +19,7 @@ type VersionOptions struct {
 }
 
 func version(options VersionOptions) error {
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight)
-	defer w.Flush()
-
-	clientVersion := internal.Version
+	clientSemVer := fmt.Sprintf("v%s", internal.Version)
 	serverVersion := "not connected"
 
 	// Note that we use the client to get this version, but it is in fact the server version
@@ -31,24 +31,62 @@ func version(options VersionOptions) error {
 		}
 	}
 
-	clientSemVer := fmt.Sprintf("v%s", serverVersion)
-	serverSemVer := fmt.Sprintf("v%s", clientVersion)
+	serverSemVer := fmt.Sprintf("v%s", serverVersion)
 
-	if semver.Compare(clientSemVer, serverSemVer) > 0 {
-		fmt.Fprintf(w, "ERROR: Your client (%s) is out of date. Please update to %s.\n", clientVersion, serverVersion)
+	err = checkUpdate(clientSemVer, serverSemVer)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed checking for updates:", err.Error())
 	}
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight)
+	defer w.Flush()
 
 	fmt.Fprintln(w)
 
 	if !options.Registry {
-		fmt.Fprintln(w, "Client:\t", clientVersion)
+		fmt.Fprintln(w, "Client:\t", clientSemVer)
 	}
 
 	if !options.Client {
-		fmt.Fprintln(w, "Registry:\t", serverVersion)
+		fmt.Fprintln(w, "Registry:\t", serverSemVer)
 	}
 
 	fmt.Fprintln(w)
+
+	return nil
+}
+
+func checkUpdate(clientSemVer, serverSemVer string) error {
+	latestSemVer := "nonexistent"
+
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, "https://releases.infrahq.com/infra/latest/version", nil)
+	if err != nil {
+		return err
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode >= 400 {
+		return fmt.Errorf("%s", res.Status)
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err == nil {
+		latestSemVer = strings.TrimSpace(string(body))
+	}
+
+	if semver.Compare(latestSemVer, clientSemVer) > 0 {
+		fmt.Fprintf(os.Stderr, "Your client (%s) is out of date. Please update to %s.\n", clientSemVer, latestSemVer)
+	}
+
+	if semver.IsValid(serverSemVer) && semver.Compare(latestSemVer, serverSemVer) > 0 {
+		fmt.Fprintf(os.Stderr, "Your server (%s) is out of date. Please update to %s.\n", serverSemVer, latestSemVer)
+	}
 
 	return nil
 }
