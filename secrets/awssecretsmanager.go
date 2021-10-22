c3ded67ddfa269ba222d000d1cf40e739814071b
Steven Soroka <steven@infrahq.com>
2021-10-22 00:09:19 -0400

    add aws secrets manager

---
+++ secrets/awssecretsmanager.go
@@ -0,0 +1,77 @@
+package secrets
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
+)
+
+var _ SecretStorage = &AWSSecretsManager{}
+
+type AWSSecretsManager struct {
+	UseSecretMaps bool // TODO: support storing to json maps if this is enabled.
+
+	client *secretsmanager.SecretsManager
+}
+
+func NewAWSSecretsManager(client *secretsmanager.SecretsManager) *AWSSecretsManager {
+	return &AWSSecretsManager{
+		client: client,
+	}
+}
+
+// SetSecret
+// must have the secretsmanager:CreateSecret permission
+// if using tags, must have secretsmanager:TagResource
+// if using kms customer-managed keys, also need:
+// - kms:GenerateDataKey
+// - kms:Decrypt
+func (s *AWSSecretsManager) SetSecret(name string, secret []byte) error {
+	name = strings.ReplaceAll(name, ":", "_")
+	_, err := s.client.CreateSecretWithContext(context.TODO(), &secretsmanager.CreateSecretInput{
+		Name:         &name,
+		SecretBinary: secret,
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case secretsmanager.ErrCodeResourceExistsException:
+				// try replacing instead
+				_, err = s.client.UpdateSecretWithContext(context.TODO(), &secretsmanager.UpdateSecretInput{
+					SecretBinary: secret,
+					SecretId:     &name,
+				})
+				if err != nil {
+					return fmt.Errorf("update secret: %w", err)
+				}
+				return nil
+			}
+		}
+
+		return fmt.Errorf("creating secret: %w", err)
+	}
+	return nil
+}
+
+// GetSecret
+// must have permission secretsmanager:GetSecretValue
+// kms:Decrypt - required only if you use a customer-managed Amazon Web Services KMS key to encrypt the secret
+func (s *AWSSecretsManager) GetSecret(name string) (secret []byte, err error) {
+	name = strings.ReplaceAll(name, ":", "_")
+	sec, err := s.client.GetSecretValueWithContext(context.TODO(), &secretsmanager.GetSecretValueInput{
+		SecretId: &name,
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case secretsmanager.ErrCodeResourceNotFoundException:
+				return nil, nil
+			}
+		}
+		return nil, fmt.Errorf("get secret: %w", err)
+	}
+	return sec.SecretBinary, nil
+}
