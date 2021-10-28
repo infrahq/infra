a6eb85fe857661acfad610f838bdae2567401251
Michael Yang <michael.yang@infrahq.com>
2021-10-28 10:54:01 -0700

    logout of all registries by default (#541)
    * logout of all registries by default
    
    * logout: debug logging

--- internal/cmd/logout.go
+++ internal/cmd/logout.go
@@ -6,39 +6,70 @@ import (
 
 	"github.com/infrahq/infra/internal"
 	"github.com/infrahq/infra/internal/api"
+	"github.com/infrahq/infra/internal/logging"
 )
 
 type LogoutOptions struct {
 	internal.GlobalOptions
 }
 
-func logout(options *LogoutOptions) error {
-	config, err := readHostConfig(options.Host)
-	if err != nil {
-		return err
-	}
-
-	if config == nil {
-		return nil
-	}
-
-	client, err := NewAPIClient(config.Host, config.SkipTLSVerify)
-	if err == nil {
-		_, _ = client.AuthAPI.Logout(NewAPIContext(config.Token)).Execute()
-	}
-
-	// only clean up cache and destinations if logging out of current host
+func cleanupKubeconfig(config *ClientHostConfig) error {
 	if config.Current {
+		logging.L.Debug("cleaning up kubeconfig")
+
 		_ = updateKubeconfig(api.User{})
 
 		infraDir, err := infraHomeDir()
 		if err == nil {
+			logging.L.Debug("cleaning up cache")
 			os.RemoveAll(filepath.Join(infraDir, "cache"))
-			os.Remove(filepath.Join(infraDir, "destinations"))
 		}
 	}
 
-	_ = removeHostConfig(config.Host)
+	return removeHostConfig(config.Host)
+}
+
+func logoutOne(config *ClientHostConfig) error {
+	logging.S.Debugf("logging out %s", config.Host)
+
+	client, err := apiClientFromConfig(config.Host)
+	if err != nil {
+		logging.S.Warnf("%s", err.Error())
+		return cleanupKubeconfig(config)
+	}
+
+	ctx, err := apiContextFromConfig(config.Host)
+	if err != nil {
+		logging.S.Warnf("%s", err.Error())
+		return cleanupKubeconfig(config)
+	}
+
+	_, err = client.AuthAPI.Logout(ctx).Execute()
+	if err != nil {
+		logging.S.Warnf("%s", err.Error())
+		return cleanupKubeconfig(config)
+	}
+
+	return cleanupKubeconfig(config)
+}
+
+func logout(options *LogoutOptions) error {
+	if options.Host == "" {
+		configs, _ := readConfig()
+		for i := range configs.Hosts {
+			_ = logoutOne(&configs.Hosts[i])
+		}
+
+		return nil
+	}
+
+	config, err := readHostConfig(options.Host)
+	if err != nil {
+		logging.S.Warnf("%s", err.Error())
+		return nil
+	}
+
+	_ = logoutOne(config)
 
 	return nil
 }
