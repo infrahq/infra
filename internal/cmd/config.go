3e776c79cdec7c6fcae1885729c0dd029d9bc3e0
Bruce MacDonald <brucewmacdonald@gmail.com>
2021-10-29 09:58:53 -0400

    move migration to its own file

--- internal/cmd/config.go
+++ internal/cmd/config.go
@@ -8,36 +8,12 @@ import (
 	"path/filepath"
 )
 
-type ClientConfigV0dot1 struct {
-	Version       string `json:"version"` // always blank in v0.1
-	Name          string `json:"name"`
-	Host          string `json:"host"`
-	Token         string `json:"token"`
-	SkipTLSVerify bool   `json:"skip-tls-verify"`
-	SourceID      string `json:"source-id"`
-}
-
-type ClientConfigV0dot2 struct {
-	Version string                   `json:"version"` // always 0.2 in v0.2
-	Hosts   []ClientHostConfigV0dot2 `json:"hosts"`
-}
-
 // current: v0.3
 type ClientConfig struct {
 	Version string             `json:"version"`
 	Hosts   []ClientHostConfig `json:"hosts"`
 }
 
-type ClientHostConfigV0dot2 struct {
-	Name          string `json:"name"`
-	Host          string `json:"host"`
-	Token         string `json:"token"`
-	SkipTLSVerify bool   `json:"skip-tls-verify"`
-	SourceID      string `json:"source-id"`
-	Current       bool   `json:"current"`
-}
-
-// when you change this, match the config version to the bump it was changed in, otherwise leave it
 // current: v0.3
 type ClientHostConfig struct {
 	Name          string `json:"name"`
@@ -157,40 +133,4 @@ func removeHostConfig(host string) error {
 	}
 
 	return nil
-}
-
-// ToV0dot2 upgrades the config to the 0.2 version
-func (c ClientConfigV0dot1) ToV0dot2() *ClientConfigV0dot2 {
-	return &ClientConfigV0dot2{
-		Version: "0.2",
-		Hosts: []ClientHostConfigV0dot2{
-			{
-				Name:          c.Name,
-				Host:          c.Host,
-				Token:         c.Token,
-				SkipTLSVerify: c.SkipTLSVerify,
-				Current:       true,
-			},
-		},
-	}
-}
-
-// ToV0dot3 upgrades the config to the 0.3 version
-func (c ClientConfigV0dot2) ToV0dot3() *ClientConfig {
-	conf := &ClientConfig{
-		Version: "0.3",
-	}
-
-	for _, h := range c.Hosts {
-		conf.Hosts = append(conf.Hosts, ClientHostConfig{
-			Name:          h.Name,
-			Host:          h.Host,
-			Token:         h.Token,
-			SkipTLSVerify: h.SkipTLSVerify,
-			ProviderID:    h.SourceID,
-			Current:       h.Current,
-		})
-	}
-
-	return conf
 }
