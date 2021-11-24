de742b77b2dc3d8b433289ba09f6c13f5c78827d
Steven Soroka <steven@infrahq.com>
2021-11-24 10:50:55 -0500

    logger filter (#657)

--- internal/logging/logger.go
+++ internal/logging/logger.go
@@ -2,9 +2,12 @@
 package logging
 
 import (
+	"bytes"
+	"encoding/json"
 	"io"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/gorilla/handlers"
 	"go.uber.org/zap"
@@ -13,8 +16,10 @@ import (
 )
 
 var (
-	L *zap.Logger        = zap.L()
-	S *zap.SugaredLogger = zap.S()
+	L, _                                    = Initialize(int(zap.InfoLevel))
+	S                   *zap.SugaredLogger  = L.Sugar()
+	defaultStderrWriter zapcore.WriteSyncer = os.Stderr
+	defaultStdoutWriter zapcore.WriteSyncer = os.Stdout
 )
 
 func Initialize(v int) (*zap.Logger, error) {
@@ -26,7 +31,7 @@ func Initialize(v int) (*zap.Logger, error) {
 	)
 
 	if term.IsTerminal(int(os.Stdin.Fd())) {
-		writer = zapcore.Lock(os.Stderr)
+		writer = zapcore.Lock(filtered(defaultStderrWriter))
 		encoder = zapcore.NewConsoleEncoder(zapcore.EncoderConfig{
 			MessageKey: "message",
 
@@ -40,7 +45,7 @@ func Initialize(v int) (*zap.Logger, error) {
 			EncodeCaller: zapcore.ShortCallerEncoder,
 		})
 	} else {
-		writer = zapcore.Lock(os.Stdout)
+		writer = zapcore.Lock(filtered(defaultStdoutWriter))
 		encoder = zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
 	}
 
@@ -64,4 +69,64 @@ func StandardErrorLog() *log.Logger {
 	}
 
 	return errorLog
+}
+
+// TODO: Remove the filtered writer after Go stops writing header
+// values to errors, as it's cpu expensive to search every log line.
+// https://github.com/golang/go/pull/48979
+func filtered(logger zapcore.WriteSyncer) zapcore.WriteSyncer {
+	return &filteredWriterSyncer{
+		dest: logger,
+	}
+}
+
+type filteredWriterSyncer struct {
+	dest zapcore.WriteSyncer
+}
+
+var strInvalidHeaderFieldValue = []byte("invalid header field value")
+
+func (w *filteredWriterSyncer) Write(b []byte) (int, error) {
+	if idx := bytes.Index(b, strInvalidHeaderFieldValue); idx >= 0 {
+		idx += len(strInvalidHeaderFieldValue)
+
+		forKeyIdx := bytes.Index(b, []byte("for key"))
+		if forKeyIdx > idx {
+			return w.dest.Write(append(b[:idx+1], b[forKeyIdx:]...))
+		}
+
+		if b[0] != '{' {
+			// not json; free to truncate.
+			return w.dest.Write(b[:idx])
+		}
+
+		// we can't see where the end is. parse the message so you can truncate it. :/
+		m := map[string]interface{}{}
+		if err := json.Unmarshal(b, &m); err != nil {
+			S.Error("Had some trouble parsing log line that needs to be filtered. Omitting log entry")
+			// on error write nothing, just to be safe.
+			return 0, nil // nolint
+		}
+
+		if msg, ok := m["msg"]; ok {
+			if smsg, ok := msg.(string); ok {
+				if idx := strings.Index(smsg, string(strInvalidHeaderFieldValue)); idx >= 0 {
+					m["msg"] = smsg[:idx+len(strInvalidHeaderFieldValue)]
+
+					newBytes, err := json.Marshal(m)
+					if err == nil {
+						return w.dest.Write(newBytes)
+					}
+				}
+			}
+		}
+		// write nothing, just to be safe.
+		return 0, nil
+	}
+
+	return w.dest.Write(b)
+}
+
+func (w *filteredWriterSyncer) Sync() error {
+	return w.dest.Sync()
 }
--- internal/logging/logger_test.go
+++ internal/logging/logger_test.go
@@ -1,9 +1,11 @@
 package logging
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 )
 
@@ -29,4 +31,54 @@ func TestConfigValidLevel(t *testing.T) {
 	if checked := logger.Check(zap.DebugLevel, "not default"); checked == nil {
 		assert.Fail(t, "could not log debug level messages")
 	}
+}
+
+func TestFiltersOutBearerTokenValue(t *testing.T) {
+	// remove this test after Go patches https://github.com/golang/go/pull/48979
+	tests := []struct {
+		Input    string
+		Expected string
+	}{
+		{
+			Input:    `could not create destination: Post \"https://{anonymized}\": net/http: invalid header field value \"Bearer {anonymized_token}\\n\" for key Authorization`,
+			Expected: `could not create destination: Post \"https://{anonymized}\": net/http: invalid header field value for key Authorization`,
+		},
+		{
+			Input:    `could not create destination: Post \"https://{anonymized}\": net/http: invalid header field value \"Bearer {anonymized_token}\\n\"`,
+			Expected: `could not create destination: Post \"https://{anonymized}\": net/http: invalid header field value`,
+		},
+		{
+			Input:    `{"level":"error","ts":1637161597.990963,"caller":"engine/engine.go:400","msg":"could not create destination: Post \"https://{anonymized}\": net/http: invalid header field value \"Bearer {anonymized_token}\\n\" for key Authorization"}`,
+			Expected: `{"level":"error","ts":1637161597.990963,"caller":"engine/engine.go:400","msg":"could not create destination: Post \"https://{anonymized}\": net/http: invalid header field value for key Authorization"}`,
+		},
+	}
+	for _, testCase := range tests {
+		writeSyncer := &testWriterSyncer{}
+		defaultStdoutWriter = writeSyncer
+		defaultStderrWriter = writeSyncer
+
+		logger, err := Initialize(int(zap.InfoLevel))
+		require.NoError(t, err)
+
+		logger.Sugar().Info(testCase.Input)
+
+		m := map[string]interface{}{}
+		err = json.Unmarshal(writeSyncer.data, &m)
+		require.NoError(t, err)
+
+		require.Equal(t, testCase.Expected, m["msg"].(string))
+	}
+}
+
+type testWriterSyncer struct {
+	data []byte
+}
+
+func (w *testWriterSyncer) Write(b []byte) (int, error) {
+	w.data = append(w.data, b...)
+	return len(b), nil
+}
+
+func (*testWriterSyncer) Sync() error {
+	return nil
 }
