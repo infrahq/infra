8664406bc690565a68be67055ac6f08077045fab
Steven Soroka <steven@infrahq.com>
2021-11-30 13:57:42 -0500

    support request timeouts (#699)

---
+++ internal/registry/middleware.go
@@ -0,0 +1,34 @@
+package registry
+
+import (
+	"context"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/infrahq/infra/internal/logging"
+)
+
+var requestTimeout = 60 * time.Second
+
+// RequestTimeoutMiddleware adds a timeout to the request context within the Gin context.
+// To correctly abort long-running requests, this depends on the users of the context to
+// stop working when the context cancels.
+// Note: The goroutine for the request is never halted; if the context is not
+// passed down to lower packages and long-running tasks, then the app will not
+// magically stop working on the request. No effort should be made to write
+// an early http response here; it's up to the users of the context to watch for
+// c.Request.Context().Err() or <-c.Request.Context().Done()
+func RequestTimeoutMiddleware(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(c.Request.Context(), requestTimeout)
+	defer cancel()
+
+	c.Request = c.Request.WithContext(ctx)
+
+	start := time.Now()
+
+	c.Next()
+
+	if elapsed := time.Since(start); elapsed > requestTimeout {
+		logging.L.Sugar().Warnf("Request to %q took %s and may have timed out", c.Request.URL.Path, elapsed)
+	}
+}
