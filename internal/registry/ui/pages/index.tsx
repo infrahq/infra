d16ba474a920f079471398fd24648bc77067ee3c
jmorganca <jmorganca@gmail.com>
2022-01-18 00:35:52 -0500

    Move ui to root directory, remove styling

--- internal/registry/ui/pages/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Index () {
-  return <h1>UI</h1>
-}
---
+++ ui/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Index () {
+  return (
+    <div>
+      You&apos;ve successfully installed Infra. To continue, see the <a className='underline' href="https://github.com/infrahq/infra">documentation</a>.
+    </div>
+  )
+}
