7985098157c1c5279d8bfaab07db203cdd72110f
Michael Yang <michael.yang@infrahq.com>
2022-02-07 15:39:30 -0800

    access

--- internal/cmd/access.go
+++ internal/cmd/access.go
@@ -8,21 +8,33 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/infrahq/infra/internal/api"
-	"github.com/infrahq/infra/uid"
 )
+
+type accessOptions struct {
+	User string `mapstructure:"user"`
+	Group string `mapstructure:"group"`
+	Provider string `mapstructure:"provider"`
+	Role string `mapstructure:"role"`
+}
 
 func newAccessListCmd() *cobra.Command {
 	return &cobra.Command{
-		Use:     "list",
+		Use:     "list [DESTINATION]",
 		Aliases: []string{"ls"},
 		Short:   "List access",
+		Args: cobra.RangeArgs(0, 1),
 		RunE: func(cmd *cobra.Command, args []string) error {
+			var resource string
+			if len(args) > 0 {
+				resource = args[0]
+			}
+
 			client, err := defaultAPIClient()
 			if err != nil {
 				return err
 			}
-	
-			grants, err := client.ListGrants(api.ListGrantsRequest{})
+
+			grants, err := client.ListGrants(api.ListGrantsRequest{Resource: resource})
 			if err != nil {
 				return err
 			}
@@ -61,154 +73,142 @@ func newAccessListCmd() *cobra.Command {
 }
 
 func newAccessGrantCmd() *cobra.Command {
-	var (
-		user     string
-		group    string
-		provider string
-		role     string
-	)
-
 	cmd := &cobra.Command{
 		Use:   "grant DESTINATION",
 		Short: "Grant access",
 		Example: `
 # Grant user admin access to a cluster
-infra grant -u suzie@acme.com -r admin kubernetes.production 
+infra access grant -u suzie@acme.com -r admin kubernetes.production
 
 # Grant group admin access to a namespace
-infra grant -g Engineering -r admin kubernetes.production.default
+infra access grant -g Engineering -r admin kubernetes.production.default
 
 # Grant user admin access to infra itself
-infra grant -u admin@acme.com -r admin infra
+infra access grant -u admin@acme.com -r admin infra
 `,
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
+			var options accessOptions
+			if err := parseOptions(cmd, &options, "INFRA_ACCESS"); err != nil {
+				return err
+			}
+
 			client, err := defaultAPIClient()
 			if err != nil {
 				return err
 			}
 
-			providers, err := client.ListProviders(provider)
+			providers, err := client.ListProviders(options.Provider)
 			if err != nil {
 				return err
 			}
 
 			if len(providers) == 0 {
-				return errors.New("No identity providers connected")
+				return errors.New("no identity providers connected")
 			}
 
 			if len(providers) > 1 {
-				return errors.New("Specify provider with -p or --provider")
+				return errors.New("specify provider with -p or --provider")
 			}
 
-			if group != "" {
-				if user != "" {
-					return errors.New("only one of -g and -u are allowed")
-				}
-
-				// create user if they don't exist
-				groups, err := client.ListGroups(api.ListGroupsRequest{Name: group})
-				if err != nil {
-					return err
-				}
-
-				var id uid.ID
-
-				if len(groups) == 0 {
-					newGroup, err := client.CreateGroup(&api.CreateGroupRequest{
-						Name:       group,
-						ProviderID: providers[0].ID,
-					})
-					if err != nil {
-						return err
-					}
-
-					id = newGroup.ID
-				} else {
-					id = groups[0].ID
-				}
-
-				_, err = client.CreateGrant(&api.CreateGrantRequest{
-					Identity:  fmt.Sprintf("g:%s", id),
-					Resource:  args[0],
-					Privilege: role,
-				})
-				if err != nil {
-					return err
-				}
+			if options.User != "" && options.Group != "" {
+				return errors.New("only allowed one of --user or --group")
 			}
 
-			if user != "" {
-				if group != "" {
-					return errors.New("only one of -g and -u are allowed")
-				}
+			if options.Role == "" {
+				return errors.New("specific role with -r or --role")
+			}
 
+			var id strings.Builder
+
+			if options.User != "" {
 				// create user if they don't exist
-				users, err := client.ListUsers(api.ListUsersRequest{Email: user})
+				users, err := client.ListUsers(api.ListUsersRequest{Email: options.User})
 				if err != nil {
 					return err
 				}
 
-				var id uid.ID
+				id.WriteString("u:")
 
 				if len(users) == 0 {
 					newUser, err := client.CreateUser(&api.CreateUserRequest{
-						Email:      user,
+						Email:      options.User,
 						ProviderID: providers[0].ID,
 					})
 					if err != nil {
 						return err
 					}
 
-					id = newUser.ID
+					id.WriteString(newUser.ID.String())
 				} else {
-					id = users[0].ID
-				}
-
-				_, err = client.CreateGrant(&api.CreateGrantRequest{
-					Identity:  fmt.Sprintf("u:%s", id),
-					Resource:  args[0],
-					Privilege: role,
-				})
-
-				if err != nil {
-					return err
+					id.WriteString(users[0].ID.String())
 				}
 			}
 
-			fmt.Println("Access granted")
+			if options.Group != "" {
+				// create group if they don't exist
+				groups, err := client.ListGroups(api.ListGroupsRequest{Name: options.Group})
+				if err != nil {
+					return err
+				}
+
+				id.WriteString("g:")
+
+				if len(groups) == 0 {
+					newGroup, err := client.CreateGroup(&api.CreateGroupRequest{
+						Name:       options.Group,
+						ProviderID: providers[0].ID,
+					})
+					if err != nil {
+						return err
+					}
+
+					id.WriteString(newGroup.ID.String())
+				} else {
+					id.WriteString(groups[0].ID.String())
+				}
+			}
+
+			_, err = client.CreateGrant(&api.CreateGrantRequest{
+				Identity:  id.String(),
+				Privilege: options.Role,
+				Resource:  args[0],
+			})
+			if err != nil {
+				return err
+			}
+
+			fmt.Println("Access granted!")
 
 			return nil
 		},
 	}
 
-	cmd.Flags().StringVarP(&user, "user", "u", "", "User to grant access to")
-	cmd.Flags().StringVarP(&group, "group", "g", "", "Group to grant access to")
-	cmd.Flags().StringVarP(&provider, "provider", "p", "", "Provider from which to grant user access to")
-	cmd.Flags().StringVarP(&role, "role", "r", "", "Role to grant")
+	cmd.Flags().StringP("user", "u", "", "User to grant access to")
+	cmd.Flags().StringP("group", "g", "", "Group to grant access to")
+	cmd.Flags().StringP("provider", "p", "", "Provider from which to grant user access to")
+	cmd.Flags().StringP("role", "r", "", "Role to grant")
 
 	return cmd
 }
 
 func newAccessRevokeCmd() *cobra.Command {
-	var (
-		user     string
-		group    string
-		provider string
-		role     string
-	)
-
 	cmd := &cobra.Command{
 		Use:   "revoke DESTINATION",
 		Short: "Revoke access",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
+			var options accessOptions
+			if err := parseOptions(cmd, &options, "INFRA_ACCESS"); err != nil {
+				return err
+			}
+
 			client, err := defaultAPIClient()
 			if err != nil {
 				return err
 			}
 
-			providers, err := client.ListProviders(provider)
+			providers, err := client.ListProviders(options.Provider)
 			if err != nil {
 				return err
 			}
@@ -221,31 +221,14 @@ func newAccessRevokeCmd() *cobra.Command {
 				return errors.New("Specify provider with -p or --provider")
 			}
 
-			var identity string
-
-			if group != "" {
-				if user != "" {
-					return errors.New("only one of -g and -u are allowed")
-				}
-
-				groups, err := client.ListGroups(api.ListGroupsRequest{Name: group})
-				if err != nil {
-					return err
-				}
-
-				if len(groups) == 0 {
-					return errors.New("no such group")
-				}
-
-				identity = "g:" + groups[0].ID.String()
+			if options.User != "" && options.Group != "" {
+				return errors.New("only allowed one of --user or --group")
 			}
 
-			if user != "" {
-				if group != "" {
-					return errors.New("only one of -g and -u are allowed")
-				}
+			var id string
 
-				users, err := client.ListUsers(api.ListUsersRequest{Email: user})
+			if options.User != "" {
+				users, err := client.ListUsers(api.ListUsersRequest{Email: options.User})
 				if err != nil {
 					return err
 				}
@@ -254,10 +237,27 @@ func newAccessRevokeCmd() *cobra.Command {
 					return errors.New("no such user")
 				}
 
-				identity = "u:" + users[0].ID.String()
+				id = fmt.Sprintf("u:%s", users[0].ID.String())
 			}
 
-			grants, err := client.ListGrants(api.ListGrantsRequest{Resource: args[0], Identity: identity, Privilege: role})
+			if options.Group != "" {
+				groups, err := client.ListGroups(api.ListGroupsRequest{Name: options.Group})
+				if err != nil {
+					return err
+				}
+
+				if len(groups) == 0 {
+					return errors.New("no such group")
+				}
+
+				id = fmt.Sprintf("g:%s", groups[0].ID.String())
+			}
+
+			grants, err := client.ListGrants(api.ListGrantsRequest{
+				Identity: id,
+				Privilege: options.Role,
+				Resource: args[0],
+			})
 			if err != nil {
 				return err
 			}
@@ -269,16 +269,16 @@ func newAccessRevokeCmd() *cobra.Command {
 				}
 			}
 
-			fmt.Println("Access revoked")
+			fmt.Println("Access revoked!")
 
 			return nil
 		},
 	}
 
-	cmd.Flags().StringVarP(&user, "user", "u", "", "User to revoke access from")
-	cmd.Flags().StringVarP(&group, "group", "g", "", "Group to revoke access from")
-	cmd.Flags().StringVarP(&provider, "provider", "p", "", "Provider from which to revoke access from")
-	cmd.Flags().StringVarP(&role, "role", "r", "", "Role to revoke")
+	cmd.Flags().StringP("user", "u", "", "User to revoke access from")
+	cmd.Flags().StringP("group", "g", "", "Group to revoke access from")
+	cmd.Flags().StringP("provider", "p", "", "Provider from which to revoke access from")
+	cmd.Flags().StringP("role", "r", "", "Role to revoke")
 
 	return cmd
 }
