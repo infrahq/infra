ec011f89dfec79bd09d87eb6c284ec526788c5f4
kimskimchi <elizabethkim@infrahq.com>
2022-03-15 07:03:47 -0400

    refactor(cli): moved info

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"text/tabwriter"
 	"time"
 
 	"github.com/AlecAivazis/survey/v2/terminal"
@@ -474,62 +473,7 @@ func newInfoCmd() *cobra.Command {
 		Hidden:  true,
 		PreRunE: mustBeLoggedInC,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			config, err := currentHostConfig()
-			if err != nil {
-				return err
-			}
-
-			client, err := defaultAPIClient()
-			if err != nil {
-				return err
-			}
-
-			w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight)
-			defer w.Flush()
-
-			if config.PolymorphicID.IsUser() {
-				provider, err := client.GetProvider(config.ProviderID)
-				if err != nil {
-					return err
-				}
-
-				user, err := client.GetUser(config.ID)
-				if err != nil {
-					return err
-				}
-
-				groups, err := client.ListUserGroups(config.ID)
-				if err != nil {
-					return err
-				}
-
-				var groupsStr string
-				for i, g := range groups {
-					if i != 0 {
-						groupsStr += ", "
-					}
-
-					groupsStr += g.Name
-				}
-
-				fmt.Fprintln(w)
-				fmt.Fprintln(w, "Server:\t", config.Host)
-				fmt.Fprintf(w, "Identity Provider:\t %s (%s)\n", provider.Name, provider.URL)
-				fmt.Fprintln(w, "User:\t", user.Email)
-				fmt.Fprintln(w)
-			} else if config.PolymorphicID.IsMachine() {
-				machine, err := client.GetMachine(config.ID)
-				if err != nil {
-					return err
-				}
-
-				fmt.Fprintln(w)
-				fmt.Fprintln(w, "Server:\t", config.Host)
-				fmt.Fprintln(w, "Machine User:\t", machine.Name)
-				fmt.Fprintln(w)
-			}
-
-			return nil
+			return info()
 		},
 	}
 }
