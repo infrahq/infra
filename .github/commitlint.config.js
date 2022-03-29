976803bbfd3b4c448bbfabd68442356b6da53959
Daniel Nephin <dnephin@gmail.com>
2022-03-29 18:38:34 -0400

    improve(ci): propose a smaller set of commit types

--- .github/commitlint.config.js
+++ .github/commitlint.config.js
@@ -5,17 +5,10 @@ module.exports = {
             2,
             'always',
             [
-                'build',
-                'chore',
-                'ci',
-                'docs',
                 'feat',
                 'fix',
-                'perf',
-                'refactor',
-                'revert',
-                'style',
-                'test',
+                'improve',
+                'maintain',
             ]
         ]
     }
