98e57d9dc058232bb48ce3a77a4a9c020087e738
Steven Soroka <steven@infrahq.com>
2022-03-30 12:53:26 -0400

    fix: only init schema if it's never been done

--- internal/server/data/migrations.go
+++ internal/server/data/migrations.go
@@ -42,7 +42,12 @@ func migrate(db *gorm.DB) error {
 		// next one here
 	})
 
-	m.InitSchema(automigrate)
+	m.InitSchema(func(db *gorm.DB) error {
+		if db.Migrator().HasTable("users") {
+			return nil
+		}
+		return automigrate(db)
+	})	
 
 	if err := m.Migrate(); err != nil {
 		return err
