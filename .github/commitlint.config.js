38d0e54351c953dbe1908a951d6a886a7df6db9d
Daniel Nephin <dnephin@gmail.com>
2022-03-30 20:18:06 -0400

    improve(ci): document the commitlint types
    and reference them from the contributing doc.

--- .github/commitlint.config.js
+++ .github/commitlint.config.js
@@ -5,9 +5,14 @@ module.exports = {
             2,
             'always',
             [
+                // feat is used for a new feature or enhancement and will increment the minor version.
                 'feat',
+                // fix is used for bug fixes and will increment the patch version.
                 'fix',
+                // improvement is used for quality improvements, refactors, and changes to docs.
                 'improve',
+                // maintain is used for maintenance tasks like updating dependencies,
+                // releasing a new version, or reverting a change.
                 'maintain',
             ]
         ],
