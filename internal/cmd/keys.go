41e22b2e8d464b30460d3ed11d6c7776115c3b26
kimskimchi <elizabethkim@infrahq.com>
2022-04-10 00:29:15 -0400

    improve: remove IDs for keys-list

--- internal/cmd/keys.go
+++ internal/cmd/keys.go
@@ -176,7 +176,6 @@ func newKeysListCmd() *cobra.Command {
 			}
 
 			type row struct {
-				ID                string `header:"ID"`
 				Name              string `header:"NAME"`
 				IssuedFor         string `header:"ISSUED FOR"`
 				Created           string `header:"CREATED"`
@@ -187,7 +186,6 @@ func newKeysListCmd() *cobra.Command {
 			var rows []row
 			for _, k := range keys {
 				rows = append(rows, row{
-					ID:                k.ID.String(),
 					Name:              k.Name,
 					IssuedFor:         k.IssuedFor.String(),
 					Created:           prettytime.Format(time.Time(k.Created)),
