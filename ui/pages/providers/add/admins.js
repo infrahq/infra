845001fac6a17c5e1b7415ea7c726b2cc0a476f3
Eva Ho <eva.ho@infrahq.com>
2022-04-13 11:58:38 -0400

    fix: in progress fixing providers

--- ui/pages/providers/add/admins.js
+++ ui/pages/providers/add/admins.js
@@ -11,7 +11,7 @@ import AddAdmin from '../../../components/providers/okta/AddAdmin'
 import AuthContext from '../../../store/AuthContext'
 
 const grantAdminAccess = async (userId) => {
-  await axios.post('/v1/grants', { identity: userId, resource: 'infra', privilege: 'admin' })
+  await axios.post('/v1/grants', { subject: userId, resource: 'infra', privilege: 'admin' })
     .then(async () => {
       await Router.push({ pathname: '/providers' }, undefined, { shallow: true })
     }).catch((error) => {
@@ -30,17 +30,15 @@ const Admins = () => {
 
   const moveToNext = async () => {
     const providerId = newestProvider && newestProvider.id
-    const params = {
-      email: adminEmail,
-      provider_id: providerId
-    }
 
-    await axios.get('/v1/users', { params })
+    await axios.get(`/v1/identities?name=${adminEmail}`)
       .then(async (response) => {
+        console.log('response:', response)
         if (response.data.length === 0) {
-          await axios.post('/v1/users',
-            { email: adminEmail, providerID: providerId })
+          await axios.post('/v1/identities',
+            { name: adminEmail, providerID: providerId, kind: 'user' })
             .then(async (response) => {
+              console.log('response when post: ', response)
               await grantAdminAccess(response.data.id)
             }).catch((error) => {
               console.log(error)
