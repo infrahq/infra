8afcf1573347b35af63b53fd5d6cae6e4897da3c
Jeffrey Morgan <jmorganca@gmail.com>
2022-04-20 20:17:41 -0400

    fix header on settings page

--- ui/pages/settings/index.js
+++ ui/pages/settings/index.js
@@ -12,7 +12,9 @@ export default function () {
       </Head>
       <div className='flex flex-row mt-4 lg:mt-6'>
         {/* TODO: icon path is wrong */}
-        <HeaderIcon iconPath='/destinations-color.svg' />
+        <div className='mt-2 mr-6'>
+          <HeaderIcon width={6} iconPath='/destinations-color.svg' />
+        </div>
         <div className='flex-1 flex flex-col space-y-4'>
           <h1 className='text-2xl font-bold mt-6 mb-4'>Settings</h1>
           <div className='pt-3'>
