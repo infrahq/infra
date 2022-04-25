5f6e3c7ffc74ca14d7d6c72d6b5cc35e8ddf1203
Daniel Nephin <dnephin@infrahq.com>
2022-04-25 14:15:38 -0400

    improve(cli): add a test for keys list

--- internal/cmd/keys_test.go
+++ internal/cmd/keys_test.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"gotest.tools/v3/assert"
+	"gotest.tools/v3/golden"
 
 	"github.com/infrahq/infra/api"
 	"github.com/infrahq/infra/uid"
@@ -25,6 +26,10 @@ func TestKeysAddCmd(t *testing.T) {
 		handler := func(resp http.ResponseWriter, req *http.Request) {
 			// the command does a lookup for machine ID
 			if requestMatches(req, http.MethodGet, "/v1/identities") {
+				if req.URL.Query().Get("name") != "my-machine" {
+					resp.WriteHeader(http.StatusBadRequest)
+					return
+				}
 				resp.WriteHeader(http.StatusOK)
 				err := json.NewEncoder(resp).Encode([]*api.Identity{
 					{ID: uid.ID(12345678)},
@@ -87,4 +92,103 @@ func TestKeysAddCmd(t *testing.T) {
 
 func requestMatches(req *http.Request, method string, path string) bool {
 	return req.Method == method && req.URL.Path == path
+}
+
+func TestKeysListCmd(t *testing.T) {
+	home := t.TempDir()
+	t.Setenv("HOME", home)
+	t.Setenv("USERPROFILE", home) // for windows
+
+	base := time.Now().Add(-24 * time.Hour)
+
+	setup := func(t *testing.T) {
+		handler := func(resp http.ResponseWriter, req *http.Request) {
+			query := req.URL.Query()
+
+			// the command does a lookup for machine ID
+			if requestMatches(req, http.MethodGet, "/v1/identities") {
+				if query.Get("name") != "my-machine" {
+					resp.WriteHeader(http.StatusBadRequest)
+					return
+				}
+				resp.WriteHeader(http.StatusOK)
+				err := json.NewEncoder(resp).Encode([]*api.Identity{
+					{ID: uid.ID(12345678)},
+				})
+				assert.Check(t, err)
+				return
+			}
+
+			if !requestMatches(req, http.MethodGet, "/v1/access-keys") {
+				resp.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			resp.WriteHeader(http.StatusOK)
+			if query.Get("identity_id") == uid.ID(12345678).String() {
+				err := json.NewEncoder(resp).Encode([]api.AccessKey{
+					{
+						Name:          "machine-key",
+						IssuedFor:     uid.ID(12345678),
+						IssuedForName: "my-machine",
+						Created:       api.Time(base.Add(5 * time.Minute)),
+						Expires:       api.Time(base.Add(30 * time.Hour)),
+					},
+				})
+				assert.Check(t, err)
+				return
+			}
+			err := json.NewEncoder(resp).Encode([]api.AccessKey{
+				{
+					Name:          "front-door",
+					IssuedFor:     uid.ID(12345),
+					IssuedForName: "admin",
+					Created:       api.Time(base.Add(time.Minute)),
+				},
+				{
+					Name:              "side-door",
+					IssuedFor:         uid.ID(12345),
+					IssuedForName:     "admin",
+					Created:           api.Time(base.Add(time.Minute)),
+					Expires:           api.Time(base.Add(30 * time.Hour)),
+					ExtensionDeadline: api.Time(base.Add(50 * time.Hour)),
+				},
+				{
+					Name:          "storage",
+					IssuedFor:     uid.ID(12349),
+					IssuedForName: "clerk",
+					Created:       api.Time(base.Add(4 * time.Hour)),
+					Expires:       api.Time(base.Add(30 * time.Hour)),
+				},
+			})
+			assert.Check(t, err)
+		}
+
+		srv := httptest.NewTLSServer(http.HandlerFunc(handler))
+		t.Cleanup(srv.Close)
+
+		cfg := newTestClientConfig(srv, api.Identity{})
+		err := writeConfig(&cfg)
+		assert.NilError(t, err)
+	}
+
+	t.Run("list all", func(t *testing.T) {
+		setup(t)
+		ctx, bufs := PatchCLI(context.Background())
+
+		err := Run(ctx, "keys", "list")
+		assert.NilError(t, err)
+
+		golden.Assert(t, bufs.Stdout.String(), t.Name())
+	})
+
+	t.Run("filter by machine name", func(t *testing.T) {
+		setup(t)
+		ctx, bufs := PatchCLI(context.Background())
+
+		err := Run(ctx, "keys", "list", "--machine", "my-machine")
+		assert.NilError(t, err)
+
+		golden.Assert(t, bufs.Stdout.String(), t.Name())
+	})
 }
