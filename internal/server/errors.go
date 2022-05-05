7b331d6560d318fddbdee61e91ed5125e6e3d4d6
Daniel Nephin <dnephin@infrahq.com>
2022-05-05 13:17:04 -0400

    improve: remove method receiver
    And remove unused method.

--- internal/server/errors.go
+++ internal/server/errors.go
@@ -20,7 +20,7 @@ import (
 // sendAPIError translates err into the appropriate HTTP status code, builds a
 // response body using api.Error, then sends both as a response to the active
 // request.
-func (a *API) sendAPIError(c *gin.Context, err error) {
+func sendAPIError(c *gin.Context, err error) {
 	resp := &api.Error{
 		Code:    http.StatusInternalServerError,
 		Message: "internal server error", // don't leak any info by default
