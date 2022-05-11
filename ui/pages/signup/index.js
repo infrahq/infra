cc3874ef0bf35ab3ac7353041338b0a0d43f5ac1
hoyyeva <63033505+hoyyeva@users.noreply.github.com>
2022-05-11 13:25:46 -0400

    feat: login layout updates (#1932)
    * feat signup page
    
    * feat: login page wip
    
    * feat: update login and signup pages
    
    * fix: address comments

--- ui/pages/signup/index.js
+++ ui/pages/signup/index.js
@@ -68,24 +68,42 @@ export default function () {
   }
 
   return (
-    <div className='flex flex-col justify-center items-center h-full w-full max-w-md mx-auto mb-48'>
-      <HeaderIcon size={12} iconPath='/infra-color.svg' />
-      <h1 className='mt-5 text-base font-bold'>Welcome to Infra</h1>
-      <h2 className='text-sm text-center max-w-xs my-2 text-gray-300'>You've successfully installed Infra.<br />Set up your admin user to get started.</h2>
-      <form onSubmit={onSubmit} className='flex flex-col w-full max-w-sm my-8'>
-        <input autoFocus type='email' placeholder='Email' onChange={e => setEmail(e.target.value)} className={`bg-purple-100/5 border border-zinc-800 text-sm px-5 mt-2 py-3 rounded-full focus:outline-none focus:ring focus:ring-cyan-600 ${errors.email ? 'border-pink-500' : ''}`} />
-        {errors.email && <ErrorMessage message={errors.email} />}
-
-        <input type='password' placeholder='Password' onChange={e => setPassword(e.target.value)} className={`bg-purple-100/5 border border-zinc-800 text-sm px-5 mt-2 py-3 rounded-full focus:outline-none focus:ring focus:ring-cyan-600 ${errors.password ? 'border-pink-500' : ''}`} />
-        {errors.password && <ErrorMessage message={errors.password} />}
-
-        <button disabled={!email || !password} className='bg-gradient-to-tr mt-5 from-indigo-300 to-pink-100 hover:from-indigo-200 hover:to-pink-50 rounded-full p-0.5 my-2 disabled:opacity-30'>
-          <div className='bg-black rounded-full text-sm px-4 py-3'>
-            Get Started
+    <div className='h-auto w-full max-w-sm mx-auto overflow-hidden'>
+      <div className='flex flex-col justify-center items-center px-5 py-5 mt-40 border rounded-lg border-gray-950'>
+        <HeaderIcon size={12} iconPath='/infra-color.svg' />
+        <h1 className='text-header font-bold'>Welcome to Infra</h1>
+        <h2 className='text-title text-center max-w-md my-3 text-gray-300'>You've successfully installed Infra.<br />Set up your admin user to get started.</h2>
+        <form onSubmit={onSubmit} className='flex flex-col w-full max-w-sm my-8'>
+          <div className='w-full my-4'>
+            <div className='text-label text-gray-200 uppercase'>Email</div>
+            <input
+              autoFocus
+              type='email'
+              placeholder='email@address.com'
+              onChange={e => setEmail(e.target.value)}
+              className={`w-full bg-transparent border-b border-gray-950 text-name px-px mt-2 py-3 focus:outline-none focus:border-b focus:border-gray-200 placeholder:italic ${errors.email ? 'border-pink-300' : ''}`}
+            />
+            {errors.email && <ErrorMessage message={errors.email} />}
           </div>
-          {error && <ErrorMessage message={error} center />}
-        </button>
-      </form>
+          <div className='w-full my-4'>
+            <div className='text-label text-gray-200 uppercase'>Password</div>
+            <input
+              type='password'
+              placeholder='enter your password'
+              onChange={e => setPassword(e.target.value)}
+              className={`w-full bg-transparent border-b border-gray-950 text-name px-px mt-2 py-3 focus:outline-none focus:border-b focus:border-gray-200 placeholder:italic ${errors.password ? 'border-pink-300' : ''}`}
+            />
+            {errors.password && <ErrorMessage message={errors.password} />}
+          </div>
+
+          <button disabled={!email || !password} className='bg-gradient-to-tr mt-5 from-indigo-300 to-pink-100 hover:from-indigo-200 hover:to-pink-50 rounded-md p-0.5 my-2 disabled:opacity-30'>
+            <div className='bg-black text-purple-50 rounded-md text-name px-4 py-3'>
+              Get Started
+            </div>
+            {error && <ErrorMessage message={error} center />}
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
