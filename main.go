d2093afe25efa3134a417f9f977dc88506eca06e
kimskimchi <elizabethkim@infrahq.com>
2022-05-11 12:05:48 -0400

    improve: support cli error struct

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"os"
 
 	"github.com/AlecAivazis/survey/v2/terminal"
@@ -13,9 +14,17 @@ import (
 
 func main() {
 	if err := cmd.Run(context.Background(), os.Args[1:]...); err != nil {
-		if !errors.Is(err, terminal.InterruptErr) {
-			logging.S.Error(err.Error())
+		var userErr cmd.Error
+		switch {
+		case errors.Is(err, terminal.InterruptErr):
+			logging.S.Debug("user interrupted (kill, stop) the process")
+		case errors.As(err, &userErr):
+			fmt.Fprintln(os.Stderr, userErr.Error())
+		default:
+			userErr.OriginalError = err
+			fmt.Fprintln(os.Stderr, userErr.Error())
 		}
+
 		os.Exit(1)
 	}
 }
