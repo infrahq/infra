fcb2512f1cd8f747b649ac17769da649a1f8291d
hoyyeva <63033505+hoyyeva@users.noreply.github.com>
2022-05-13 09:55:05 -0400

    update: nav and setting page layout (#1877)
    * feat: nav layout
    
    * feat: changing layout and styling
    
    * fix: address comments and handle long user name
    
    * fix: updating setting page
    
    * fix: address comments
    
    * fix: update layout
    
    * feat: update setting layout
    
    * feat: nav and setting change layout
    
    * fix: nav cutoff on browser
    
    * feat: nav working in progress
    
    * fix: layout updated
    
    * feat: add empty state
    
    * fix: ran standard
    
    * feat: login wip
    
    * fix: address comments
    
    * fix: versioning

--- ui/components/error-message.js
+++ ui/components/error-message.js
@@ -1,5 +1,5 @@
 export default function ({ message, center = false }) {
   return (
-    <p className={`${center ? 'mt-2 text-center' : 'px-4 mb-1'} text-sm text-pink-500`}>{message}</p>
+    <p className={`${center ? 'mt-2 text-center' : 'mb-1'} text-sm text-pink-500`}>{message}</p>
   )
 }
