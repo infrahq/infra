6c910b8b36140e6f12aadd7f1ff5f2b9a18da97f
Bruce MacDonald <brucewmacdonald@gmail.com>
2022-05-20 10:53:32 -0400

    feat: agent to keep kube config up to date

--- internal/cmd/agent_test.go
+++ internal/cmd/agent_test.go
@@ -10,19 +10,24 @@ import (
 func TestProcessRunning(t *testing.T) {
 	type testCase struct {
 		pid      int
+		err      string
 		expected bool
 	}
 
 	testCases := []testCase{
-		{pid: os.Getpid(), expected: true},
-		{pid: -1, expected: false}, // invalid pid
-		{pid: 0, expected: false},  // default config pid, also invalid
+		{pid: os.Getpid(), err: "", expected: true},
+		{pid: -1, err: "invalid pid", expected: false}, // invalid pid
+		{pid: 0, err: "", expected: false},             // default config pid, also invalid
 	}
 
 	for _, tc := range testCases {
 		result, err := processRunning(int32(tc.pid))
 
-		assert.NilError(t, err)
+		if tc.err != "" {
+			assert.ErrorContains(t, err, tc.err)
+		} else {
+			assert.NilError(t, err)
+		}
 		assert.Equal(t, result, tc.expected)
 	}
 }
