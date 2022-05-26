1dbf7fa832b7b282a624c18176c018ca8a7a0e11
Jeffrey Morgan <251292+jmorganca@users.noreply.github.com>
2022-05-26 10:34:30 -0400

    improve: cleanup ui versioning headers (#2072)

--- ui/lib/fetch.js
+++ ui/lib/fetch.js
@@ -5,12 +5,12 @@ const base = '0.13.0'
 // Patch the global fetch to include our base API
 // version for requests to the same domain
 global.fetch = (resource, info) => fetch(resource, {
-  ...info,
   ...resource.startsWith('/')
     ? {
         headers: {
           'Infra-Version': base
         }
       }
-    : {}
+    : {},
+  ...info
 })
