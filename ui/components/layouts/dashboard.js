65a36ebf527f1a28217d7016551cad7fbc338cb3
Jeffrey Morgan <251292+jmorganca@users.noreply.github.com>
2022-06-02 13:46:34 -0400

    fix: ui props being passed when using authrequired (#2169)

--- ui/components/layouts/dashboard.js
+++ ui/components/layouts/dashboard.js
@@ -5,7 +5,7 @@ import useSWR, { useSWRConfig } from 'swr'
 import { useAdmin } from '../../lib/admin'
 import AuthRequired from '../auth-required'
 
-function Dashboard ({ children }) {
+export default function ({ children }) {
   const router = useRouter()
   const { data: auth } = useSWR('/api/users/self')
   const { data: version } = useSWR('/api/version')
@@ -43,77 +43,69 @@ function Dashboard ({ children }) {
   }
 
   return (
-    <div className='flex h-full relative'>
-      <nav className='flex-none flex flex-col w-56 inset-y-0 overflow-hidden'>
-        <div className='flex-shrink-0 flex items-center mt-6 mb-10 lg:my-18 px-5 select-none'>
-          <Link href='/'>
-            <a><img className='h-[15px]' src='infra.svg' alt='Infra' /></a>
-          </Link>
-        </div>
-        <div className='flex-1 space-y-1 px-5 select-none'>
-          {navigation.map(n =>
-            <Link key={n.name} href={n.href}>
-              <a
-                href={n.href}
-                className={`
+    <AuthRequired>
+      <div className='flex h-full relative'>
+        <nav className='flex-none flex flex-col w-56 inset-y-0 overflow-hidden'>
+          <div className='flex-shrink-0 flex items-center mt-6 mb-10 lg:my-18 px-5 select-none'>
+            <Link href='/'>
+              <a><img className='h-[15px]' src='infra.svg' alt='Infra' /></a>
+            </Link>
+          </div>
+          <div className='flex-1 space-y-1 px-5 select-none'>
+            {navigation.map(n =>
+              <Link key={n.name} href={n.href}>
+                <a
+                  href={n.href}
+                  className={`
                   ${router.asPath.startsWith(n.href) ? 'text-white' : 'text-gray-400'}
                   rounded-lg py-2 flex items-center text-xs leading-none transition-colors duration-100
                   ${n.admin && !admin ? 'opacity-30 pointer-events-none' : ''}
                 `}
-              >
-                <img
-                  src={n.icon}
-                  className={`
+                >
+                  <img
+                    src={n.icon}
+                    className={`
                     ${router.asPath.startsWith(n.href) ? '' : 'opacity-40'}
                     mr-3 flex-shrink-0 h-[18px] w-[18px]
                   `}
-                />
-                {n.name}
-              </a>
-            </Link>
-          )}
-        </div>
-        <div className='flex group mx-2 mb-2 p-2.5 pb-1 h-12 hover:h-[132px] transition-all duration-300 ease-in-out rounded-xl bg-transparent hover:bg-gray-900 overflow-hidden'>
-          <div className='flex flex-none self-start items-stretch border border-violet-300/40 rounded-md w-[23px] h-[23px]'>
-            <div className='relative text-center flex flex-1 justify-center items-center border border-violet-300/70 text-3xs rounded-[4px] leading-none font-normal m-0.5 select-none'>
-              <span className='absolute inset-x-0 -mt-[1px]'>{auth?.name?.[0]}</span>
+                  />
+                  {n.name}
+                </a>
+              </Link>
+            )}
+          </div>
+          <div className='flex group mx-2 mb-2 p-2.5 pb-1 h-12 hover:h-[132px] transition-all duration-300 ease-in-out rounded-xl bg-transparent hover:bg-gray-900 overflow-hidden'>
+            <div className='flex flex-none self-start items-stretch border border-violet-300/40 rounded-md w-[23px] h-[23px]'>
+              <div className='relative text-center flex flex-1 justify-center items-center border border-violet-300/70 text-3xs rounded-[4px] leading-none font-normal m-0.5 select-none'>
+                <span className='absolute inset-x-0 -mt-[1px]'>{auth?.name?.[0]}</span>
+              </div>
+            </div>
+            <div className='flex-1 min-w-0 ml-1 px-2 select-none'>
+              <div className='text-gray-400 group-hover:text-white transition-colors duration-300 mt-[5px] mb-2 leading-none truncate text-2xs pb-px'>{auth?.name}</div>
+              <nav className='opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+                {subNavigation.map(s => (
+                  <Link key={s.name} href={s.href}>
+                    <a className={`w-full flex py-1.5 text-xs text-gray-400 hover:text-white ${s.admin && !admin ? 'pointer-events-none opacity-20' : ''}`}>
+                      {s.name}
+                    </a>
+                  </Link>
+                ))}
+                <button onClick={() => logout()} className='w-full text-left py-1.5 text-gray-400 text-xs hover:text-white cursor-pointer'>
+                  Sign Out
+                </button>
+                <div className='text-3xs mt-2 leading-none text-violet-50/40'>
+                  Infra version <span className='font-mono select-text'>{version?.version}</span>
+                </div>
+              </nav>
             </div>
           </div>
-          <div className='flex-1 min-w-0 ml-1 px-2 select-none'>
-            <div className='text-gray-400 group-hover:text-white transition-colors duration-300 mt-[5px] mb-2 leading-none truncate text-2xs pb-px'>{auth?.name}</div>
-            <nav className='opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
-              {subNavigation.map(s => (
-                <Link key={s.name} href={s.href}>
-                  <a className={`w-full flex py-1.5 text-xs text-gray-400 hover:text-white ${s.admin && !admin ? 'pointer-events-none opacity-20' : ''}`}>
-                    {s.name}
-                  </a>
-                </Link>
-              ))}
-              <button onClick={() => logout()} className='w-full text-left py-1.5 text-gray-400 text-xs hover:text-white cursor-pointer'>
-                Sign Out
-              </button>
-              <div className='text-3xs mt-2 leading-none text-violet-50/40'>
-                Infra version <span className='font-mono select-text'>{version?.version}</span>
-              </div>
-            </nav>
+        </nav>
+        <main className='w-full overflow-x-hidden overflow-y-scroll'>
+          <div className='mx-6 h-full'>
+            {children}
           </div>
-        </div>
-      </nav>
-      <main className='w-full overflow-x-hidden overflow-y-scroll'>
-        <div className='mx-6 h-full'>
-          {children}
-        </div>
-      </main>
-    </div>
-  )
-}
-
-export default function ({ children }) {
-  return (
-    <AuthRequired>
-      <Dashboard>
-        {children}
-      </Dashboard>
+        </main>
+      </div>
     </AuthRequired>
   )
 }
--- ui/components/layouts/fullscreen.js
+++ ui/components/layouts/fullscreen.js
@@ -3,38 +3,30 @@ import { XIcon, ChevronLeftIcon } from '@heroicons/react/outline'
 
 import AuthRequired from '../auth-required'
 
-function Fullscreen ({ children, backHref, closeHref }) {
-  return (
-    <div className='flex flex-col w-full h-full'>
-      <div className={`flex flex-none text-right justify-end ${backHref ? 'justify-between' : 'justify-end'}`}>
-        {backHref && (
-          <Link href={backHref || '/'}>
-            <a className='flex items-center p-4 text-gray-400 hover:text-white text-3xs uppercase'>
-              <ChevronLeftIcon className='w-5 h-5 mr-1.5 stroke-1' />Back
-            </a>
-          </Link>
-        )}
-        <Link href={closeHref || '/'}>
-          <a className='flex items-center p-4 text-gray-400 hover:text-white text-3xs uppercase'>
-            Close<XIcon className='w-6 h-6 ml-1 stroke-1' />
-          </a>
-        </Link>
-      </div>
-      <div className='flex-1 flex justify-center items-center mb-10'>
-        <div className='w-full max-w-xs border rounded-lg border-gray-800'>
-          {children}
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export default function ({ children }) {
+export default function ({ children, backHref, closeHref }) {
   return (
     <AuthRequired>
-      <Fullscreen>
-        {children}
-      </Fullscreen>
+      <div className='flex flex-col w-full h-full'>
+        <div className={`flex flex-none text-right justify-end ${backHref ? 'justify-between' : 'justify-end'}`}>
+          {backHref && (
+            <Link href={backHref || '/'}>
+              <a className='flex items-center p-4 text-gray-400 hover:text-white text-3xs uppercase'>
+                <ChevronLeftIcon className='w-5 h-5 mr-1.5 stroke-1' />Back
+              </a>
+            </Link>
+          )}
+          <Link href={closeHref || '/'}>
+            <a className='flex items-center p-4 text-gray-400 hover:text-white text-3xs uppercase'>
+              Close<XIcon className='w-6 h-6 ml-1 stroke-1' />
+            </a>
+          </Link>
+        </div>
+        <div className='flex-1 flex justify-center items-center mb-10'>
+          <div className='w-full max-w-xs border rounded-lg border-gray-800'>
+            {children}
+          </div>
+        </div>
+      </div>
     </AuthRequired>
   )
 }
