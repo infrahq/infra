991295cb2a2afd5a2f53da9d17ae273d2f2ce29e
Eva Ho <eva.ho@infrahq.com>
2022-06-01 23:03:36 -0400

    fix: ran standard

--- ui/components/profile-icon.js
+++ ui/components/profile-icon.js
@@ -1,6 +1,6 @@
 export default function ({ name }) {
   return (
-    <div className={`flex flex-none self-start items-stretch border border-violet-300/40 rounded-md w-7 h-7`}>
+    <div className='flex flex-none self-start items-stretch border border-violet-300/40 rounded-md w-7 h-7'>
       <div className='relative text-center flex flex-1 justify-center items-center border border-violet-300/70 text-3xs rounded-[4px] leading-none font-normal m-0.5 select-none'>
         <span className='absolute inset-x-0 -mt-[1px]'>{name}</span>
       </div>
