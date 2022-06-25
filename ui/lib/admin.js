01ca8422162bdb83b730957d283469d503107fe1
Jeffrey Morgan <jmorganca@gmail.com>
2022-06-25 10:53:03 -0400

    fix(ui): dont revalidate admin checks

--- ui/lib/admin.js
+++ ui/lib/admin.js
@@ -3,15 +3,16 @@ import useSWR from 'swr'
 const INFRA_ADMIN_ROLE = 'admin'
 
 export function useAdmin () {
-  const { data: auth } = useSWR('/api/users/self')
-  const { data: { items: grants } = {} } = useSWR(`/api/grants?user=${auth?.id}`)
+  const { data: auth } = useSWR('/api/users/self', { revalidateIfStale: false })
+  const { data: { items: grants } = {} } = useSWR(`/api/grants?user=${auth?.id}`, { revalidateIfStale: false })
+  const { data: { items: groups } = {} } = useSWR(`/api/groups?userID=${auth?.id}`, { revalidateIfStale: false })
 
   // todo: switch to using /api/grants?inherited=1 instead of
   // multiple fetches (/api/grants for each group)
-  const { data: { items: groups } = {} } = useSWR(`/api/groups?userID=${auth?.id}`)
   const { data: groupGrantDatas } = useSWR(
     () => groups.map(g => `/api/grants?group=${g.id}`) || null,
-    (...urls) => Promise.all(urls.map(url => fetch(url).then(r => r.json())))
+    (...urls) => Promise.all(urls.map(url => fetch(url).then(r => r.json()))),
+    { revalidateIfStale: false }
   )
 
   const inherited = groupGrantDatas?.map(g => g.items)?.flat()
