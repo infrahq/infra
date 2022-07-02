8c095cd0ae5ffa130ea73b97d6f76f343a353a1a
Jonathan Sneh <jsneh2@illinois.edu>
2022-07-02 07:58:52 +0300

    maintain: style

--- api/pagination.go
+++ api/pagination.go
@@ -9,7 +9,7 @@ type PaginationResponse struct {
 	Page  int `json:"page,omitempty"`
 	Limit int `json:"limit,omitempty"`
 
-	TotalCount int  `json:"total_count,omitempty"`
-	TotalPages int  `json:"total_pages,omitempty"`
-	HasNext    bool `json:"has_next,omitempty"`
+	TotalCount int  `json:"totalCount,omitempty"`
+	TotalPages int  `json:"totalPages,omitempty"`
+	HasNext    bool `json:"hasNext,omitempty"`
 }
