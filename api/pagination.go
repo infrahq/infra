92ff9fda2b25ea5d213f86b48d2b16631c6da67a
Daniel Nephin <dnephin@infrahq.com>
2022-07-06 15:45:47 -0400

    improve: add validation to pagination
    Previously the pagination validation rules were not visible from the openapi schema.
    This commit fixes the problem.

--- api/pagination.go
+++ api/pagination.go
@@ -1,8 +1,26 @@
 package api
 
+import "github.com/infrahq/infra/internal/validate"
+
 type PaginationRequest struct {
-	Page  int `form:"page" validate:"min=0"`
-	Limit int `form:"limit" validate:"min=0,max=1000"`
+	Page  int `form:"page"`
+	Limit int `form:"limit"`
+}
+
+func (p PaginationRequest) ValidationRules() []validate.ValidationRule {
+	return []validate.ValidationRule{
+		validate.IntRule{
+			Name:  "page",
+			Value: p.Page,
+			Min:   validate.Int(0),
+		},
+		validate.IntRule{
+			Name:  "limit",
+			Value: p.Limit,
+			Min:   validate.Int(0),
+			Max:   validate.Int(1000),
+		},
+	}
 }
 
 type PaginationResponse struct {
