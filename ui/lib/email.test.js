ed9798066f85a51a1032770e0d2154f8e2158caa
hoyyeva <63033505+hoyyeva@users.noreply.github.com>
2022-07-13 19:56:32 -0400

    fix: remove email validation lib (#2565)
    * fix: remove email validation lib
    
    * fix: address comments

--- ui/lib/email.test.js
+++
@@ -1,9 +0,0 @@
-import { validateEmail } from './email'
-
-test('validates email', () => {
-  expect(validateEmail('example@example.com')).toBe(true)
-})
-
-test('rejects non-email', () => {
-  expect(validateEmail('example')).toBe(false)
-})
