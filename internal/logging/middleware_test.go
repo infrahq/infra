75cbc0449d73b0732d39a54f536be70ee8c0790b
Daniel Nephin <dnephin@infrahq.com>
2022-07-13 14:13:14 -0400

    improve: update tests for logging middleware
    To be more strict about the expected log output.
    
    Also add some more test cases that use parameters in the url and different response codes.
    
    Remove asserts from the handlers, because those were only testing gin functionality and don't
    improve our confidence in the tests or middleware.

--- internal/logging/middleware_test.go
+++ internal/logging/middleware_test.go
@@ -3,6 +3,7 @@ package logging
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -19,14 +20,14 @@ func TestMiddleware(t *testing.T) {
 		router := gin.New()
 		router.Use(Middleware())
 
-		router.GET("/good", func(c *gin.Context) {})
-		router.POST("/good", func(c *gin.Context) {})
-		router.GET("/gooder", func(c *gin.Context) {})
-		router.GET("/bad", func(c *gin.Context) {
+		router.GET("/good/:id", func(c *gin.Context) {})
+		router.POST("/good/:id", func(c *gin.Context) {})
+		router.GET("/gooder/", func(c *gin.Context) {})
+		router.GET("/bad/:id", func(c *gin.Context) {
 			c.Status(http.StatusBadRequest)
 		})
 		router.GET("/broken", func(c *gin.Context) {
-			c.Status(http.StatusBadGateway)
+			c.Status(http.StatusInternalServerError)
 		})
 
 		return router
@@ -35,54 +36,67 @@ func TestMiddleware(t *testing.T) {
 	t.Run("identical requests are sampled", func(t *testing.T) {
 		b := &bytes.Buffer{}
 		router := setup(t, b)
-		router.ServeHTTP(httptest.NewRecorder(), httptest.NewRequest("GET", "/good", nil))
-		router.ServeHTTP(httptest.NewRecorder(), httptest.NewRequest("GET", "/good", nil))
+		resp := httptest.NewRecorder()
+		router.ServeHTTP(resp, httptest.NewRequest("GET", "/good/1", nil))
+		router.ServeHTTP(resp, httptest.NewRequest("GET", "/good/2", nil))
+		router.ServeHTTP(resp, httptest.NewRequest("GET", "/good/3", nil))
+		router.ServeHTTP(resp, httptest.NewRequest("GET", "/good/4", nil))
+		router.ServeHTTP(resp, httptest.NewRequest("GET", "/gooder/", nil))
+		router.ServeHTTP(resp, httptest.NewRequest("GET", "/good/5", nil))
+		router.ServeHTTP(resp, httptest.NewRequest("POST", "/good/1", nil))
+		router.ServeHTTP(resp, httptest.NewRequest("POST", "/good/2", nil))
 
-		lines := bytes.Split(bytes.TrimSpace(b.Bytes()), []byte("\n"))
-		assert.Equal(t, len(lines), 1)
-
-		good := map[string]interface{}{}
-		err := json.Unmarshal(lines[0], &good)
-		assert.NilError(t, err)
-		assert.Equal(t, good["path"], "/good")
-	})
-
-	t.Run("good and gooder", func(t *testing.T) {
-		b := &bytes.Buffer{}
-		router := setup(t, b)
-		router.ServeHTTP(httptest.NewRecorder(), httptest.NewRequest("GET", "/good", nil))
-		router.ServeHTTP(httptest.NewRecorder(), httptest.NewRequest("GET", "/gooder", nil))
-
-		lines := bytes.Split(bytes.TrimSpace(b.Bytes()), []byte("\n"))
-		assert.Equal(t, len(lines), 2)
-
-		good := map[string]interface{}{}
-		err := json.Unmarshal(lines[0], &good)
-		assert.NilError(t, err)
-		assert.Equal(t, good["path"], "/good")
-
-		gooder := map[string]interface{}{}
-		err = json.Unmarshal(lines[1], &gooder)
-		assert.NilError(t, err)
-		assert.Equal(t, gooder["path"], "/gooder")
+		actual := decodeLogs(t, b)
+		expected := []logEntry{
+			{Method: "GET", Path: "/good/1", StatusCode: 200, Level: "info"},
+			{Method: "GET", Path: "/gooder/", StatusCode: 200, Level: "info"},
+			{Method: "POST", Path: "/good/1", StatusCode: 200, Level: "info"},
+		}
+		assert.DeepEqual(t, actual, expected)
 	})
 
 	t.Run("non-200 status responses are never sampled", func(t *testing.T) {
 		b := &bytes.Buffer{}
 		router := setup(t, b)
 		resp := httptest.NewRecorder()
-		router.ServeHTTP(resp, httptest.NewRequest("GET", "/bad", nil))
-		router.ServeHTTP(resp, httptest.NewRequest("GET", "/bad", nil))
-		router.ServeHTTP(resp, httptest.NewRequest("GET", "/bad", nil))
+		router.ServeHTTP(resp, httptest.NewRequest("GET", "/bad/1", nil))
+		router.ServeHTTP(resp, httptest.NewRequest("GET", "/bad/1", nil))
+		router.ServeHTTP(resp, httptest.NewRequest("GET", "/bad/2", nil))
+		router.ServeHTTP(resp, httptest.NewRequest("GET", "/broken", nil))
+		router.ServeHTTP(resp, httptest.NewRequest("GET", "/broken", nil))
+		router.ServeHTTP(resp, httptest.NewRequest("GET", "/broken", nil))
 
-		lines := bytes.Split(bytes.TrimSpace(b.Bytes()), []byte("\n"))
-		assert.Equal(t, len(lines), 3)
-
-		for _, line := range lines {
-			bad := map[string]interface{}{}
-			err := json.Unmarshal(line, &bad)
-			assert.NilError(t, err)
-			assert.Equal(t, bad["path"], "/bad")
+		actual := decodeLogs(t, b)
+		expected := []logEntry{
+			{Method: "GET", Path: "/bad/1", StatusCode: 400, Level: "info"},
+			{Method: "GET", Path: "/bad/1", StatusCode: 400, Level: "info"},
+			{Method: "GET", Path: "/bad/2", StatusCode: 400, Level: "info"},
+			{Method: "GET", Path: "/broken", StatusCode: 500, Level: "info"},
+			{Method: "GET", Path: "/broken", StatusCode: 500, Level: "info"},
+			{Method: "GET", Path: "/broken", StatusCode: 500, Level: "info"},
 		}
+		assert.DeepEqual(t, actual, expected)
 	})
+}
+
+func decodeLogs(t *testing.T, input io.Reader) []logEntry {
+	const maxLogs = 15
+	logs := make([]logEntry, maxLogs)
+	dec := json.NewDecoder(input)
+	for i := 0; i < cap(logs); i++ {
+		err := dec.Decode(&logs[i])
+		if errors.Is(err, io.EOF) {
+			return logs[:i]
+		}
+		assert.NilError(t, err)
+	}
+	t.Errorf("more than %d logs, some were not decoded", maxLogs)
+	return logs
+}
+
+type logEntry struct {
+	Method     string `json:"method"`
+	Path       string `json:"path"`
+	StatusCode int    `json:"statusCode"`
+	Level      string `json:"level"`
 }
