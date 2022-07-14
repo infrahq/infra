fdc354c40aafc5cb45555ebf6d75197c247ae8c5
kimskimchi <elizabethkim@infrahq.com>
2022-07-14 09:18:20 -0400

    feat: add password settings columns (#2563)
    * feat: add password settings columns
    
    * improve: small
    
    * improve: small
    
    * improve: small
    
    * improve: small
    
    * improve: small
    
    * improve: small
    
    * improve: test postgres
    
    * improve: test postgres
    
    * improve: test postgres
    
    * improve: delete temp file
    
    * improve: test postgres
    
    * improve: test postgres
    
    * improve: update internal/server/data/migrationdata/202207120000-postgres.sql
    
    Co-authored-by: Bruce MacDonald <brucewmacdonald@gmail.com>
    
    * improve: test postgres
    
    * improve: test postgres
    
    Co-authored-by: Bruce MacDonald <brucewmacdonald@gmail.com>

--- internal/server/models/settings.go
+++ internal/server/models/settings.go
@@ -5,4 +5,10 @@ type Settings struct {
 
 	PrivateJWK EncryptedAtRestBytes
 	PublicJWK  []byte
+
+	LowercaseMin int `gorm:"default:0"`
+	UppercaseMin int `gorm:"default:0"`
+	NumberMin    int `gorm:"default:0"`
+	SymbolMin    int `gorm:"default:0"`
+	LengthMin    int `gorm:"default:8"`
 }
