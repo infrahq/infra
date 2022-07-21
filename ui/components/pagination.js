90d2afa322b866b9a171eab643081d57dbf55d44
Jonathan Sneh <jsneh2@illinois.edu>
2022-07-21 11:04:45 -0700

    improve: arrows into one component

--- ui/components/pagination.js
+++ ui/components/pagination.js
@@ -3,7 +3,10 @@ import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
 
 function getPageNums(selected, count, totalPages) {
   const pageNums = []
-  const beginOffset = Math.max(3, 6 + selected - totalPages)
+  const beginOffset = Math.max(
+    Math.floor(count / 2),
+    count + selected - totalPages
+  )
   for (
     let i = Math.max(1, selected - beginOffset);
     pageNums.length <= count && i <= totalPages;
@@ -29,59 +32,56 @@ function Pages({ path, selected, count, totalPages }) {
   ))
 }
 
-function LeftArrow({ path, page }) {
+function Arrow({ path, direction }) {
   return (
     <a
-      href={path + '?p=' + (page > 1 ? page - 1 : 1)}
-      className=' inline-flex items-center pr-1 text-sm font-medium text-gray-500 hover:text-violet-300 '
-    >
-      <ChevronLeftIcon
-        className=' h-5 w-5 text-gray-400 hover:text-violet-300'
-        aria-hidden='true'
-      />
-    </a>
-  )
-}
-
-function RightArrow({ path, page, maxPage }) {
-  return (
-    <a
-      href={path + '?p=' + (page < maxPage ? page + 1 : Math.max(1, maxPage))}
+      href={path}
       className='inline-flex items-center pl-1 text-sm font-medium text-gray-500 hover:text-violet-300 '
     >
-      <ChevronRightIcon
-        className='ml-3 h-5 w-5 text-gray-400 hover:text-violet-300'
-        aria-hidden='true'
-      />
+      {direction === 'RIGHT' && (
+        <ChevronRightIcon
+          className='ml-3 h-5 w-5 text-gray-400 hover:text-violet-300'
+          aria-hidden='true'
+        />
+      )}
+      {direction === 'LEFT' && (
+        <ChevronLeftIcon
+          className='ml-3 h-5 w-5 text-gray-400 hover:text-violet-300'
+          aria-hidden='true'
+        />
+      )}
     </a>
   )
 }
 
-export default function Pagination({ curr, totalPages, totalCount }) {
+export default function Pagination({
+  curr = 1,
+  totalPages = 0,
+  totalCount = 0,
+}) {
   const router = useRouter()
   const path = router.pathname
   const limit = 13 // TODO: dynamic limit
 
-  curr = curr === undefined ? 1 : parseInt(curr)
-  totalPages = totalPages === undefined ? 1 : parseInt(totalPages)
-  totalCount = totalCount === undefined ? 0 : parseInt(totalCount)
+  curr = parseInt(curr)
+  totalPages = parseInt(totalPages)
+  totalCount = parseInt(totalCount)
 
   const lowerItem = totalCount === 0 ? 0 : 1 + (curr - 1) * limit
   const upperItem = Math.min(1 + (curr - 1) * limit + limit, totalCount)
 
   return (
-    totalPages !== undefined &&
     totalPages > 1 && (
       <div>
         <nav key='paginator' className='flex justify-end px-4 pb-2'>
-          <LeftArrow path={path} page={curr}></LeftArrow>
+          <Arrow direction='LEFT' path={path + '?p=' + (curr > 1 ? curr - 1 : 1)}></Arrow>
           <Pages
             path={path}
             count={7}
             totalPages={totalPages}
             selected={curr}
           ></Pages>
-          <RightArrow path={path} page={curr} maxPage={totalPages}></RightArrow>
+          <Arrow direction='RIGHT' path={path + '?p=' + (curr < totalPages ? curr + 1 : Math.max(1, totalPages))} ></Arrow>
         </nav>
         <h3
           key='results'
