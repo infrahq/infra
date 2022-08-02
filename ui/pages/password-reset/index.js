309d1c2f3299e061d88c66330d284d2218632001
Steven Soroka <steven@infrahq.com>
2022-08-02 17:25:34 -0400

    fix: ui class name?

--- ui/pages/password-reset/index.js
+++ ui/pages/password-reset/index.js
@@ -101,9 +101,8 @@ export default function PasswordReset() {
                   setPassword(e.target.value)
                   setError('')
                 }}
-                className={`w-full border-b border-gray-800 bg-transparent px-px py-2 text-2xs placeholder:italic focus:border-b focus:outline-none focus:ring-gray-200 ${
-                  error ? 'border-pink-500/60' : ''
-                }`}
+                className={`w-full border-b border-gray-800 bg-transparent px-px py-2 text-2xs placeholder:italic focus:border-b focus:outline-none focus:ring-gray-200 ${error ? 'border-pink-500/60' : ''
+                  }`}
               />
             </div>
             <button
@@ -123,7 +122,7 @@ export default function PasswordReset() {
         <>
           <h1 className='text-base font-bold leading-snug'>Password Reset</h1>
           {submitted ? (
-            <p namespace='my-3 max-w-[260px] text-xs text-gray-300'>
+            <p className='my-3 max-w-[260px] text-xs text-gray-300'>
               Please check your email for the reset link
             </p>
           ) : (
@@ -159,9 +158,8 @@ export default function PasswordReset() {
                       setEmail(e.target.value)
                       setError('')
                     }}
-                    className={`w-full border-b border-gray-800 bg-transparent px-px py-2 text-2xs placeholder:italic focus:border-b focus:border-gray-200 focus:outline-none ${
-                      error ? 'border-pink-500/60' : ''
-                    }`}
+                    className={`w-full border-b border-gray-800 bg-transparent px-px py-2 text-2xs placeholder:italic focus:border-b focus:border-gray-200 focus:outline-none ${error ? 'border-pink-500/60' : ''
+                      }`}
                   />
                 </div>
                 <button
