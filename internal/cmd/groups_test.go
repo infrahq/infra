007c0db492160e0e9ef7c5636bfe4c01aaf39c78
Jonathan Sneh <jsneh2@illinois.edu>
2022-08-03 16:05:12 -0700

    improve: list test

--- internal/cmd/groups_test.go
+++ internal/cmd/groups_test.go
@@ -5,10 +5,13 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
-	"github.com/infrahq/infra/api"
 	"gotest.tools/v3/assert"
+	"gotest.tools/v3/golden"
+
+	"github.com/infrahq/infra/api"
 )
 
 func TestGroupAddCmd(t *testing.T) {
@@ -68,3 +71,90 @@ func TestGroupAddCmd(t *testing.T) {
 
 var expectedGroupsAddOutput = `Added group "Test"
 `
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func TestGroupsListCmd(t *testing.T) {
+	home := t.TempDir()
+	t.Setenv("HOME", home)
+	t.Setenv("USERPROFILE", home)
+
+	setup := func(t *testing.T) {
+		handler := func(resp http.ResponseWriter, req *http.Request) {
+			if requestMatches(req, http.MethodGet, "/api/users") {
+				if req.URL.Query().Get("group") == "2J" {
+					limit, err := strconv.Atoi(string(req.URL.Query().Get("limit")))
+					assert.NilError(t, err)
+
+					if limit == 0 {
+						limit = 100
+					}
+					resp.WriteHeader(http.StatusOK)
+					users := []api.User{
+						{Name: "a@example.com"},
+						{Name: "b@example.com"},
+						{Name: "c@example.com"},
+						{Name: "d@example.com"},
+						{Name: "e@example.com"},
+						{Name: "f@example.com"},
+						{Name: "g@example.com"},
+						{Name: "h@example.com"},
+						{Name: "i@example.com"}}
+
+					err = json.NewEncoder(resp).Encode(api.ListResponse[api.User]{Items: users[:min(limit, len(users))]})
+					assert.NilError(t, err)
+					return
+				}
+			}
+			if !requestMatches(req, http.MethodGet, "/api/groups") {
+				resp.WriteHeader(http.StatusTeapot)
+				return
+			}
+
+			resp.WriteHeader(http.StatusOK)
+			err := json.NewEncoder(resp).Encode(&api.ListResponse[api.Group]{Items: []api.Group{
+				{Name: "Test", ID: 100, TotalUsers: 9},
+			}})
+			assert.NilError(t, err)
+		}
+
+		srv := httptest.NewTLSServer(http.HandlerFunc(handler))
+		t.Cleanup(srv.Close)
+
+		cfg := newTestClientConfig(srv, api.User{})
+		err := writeConfig(&cfg)
+		assert.NilError(t, err)
+	}
+
+	t.Run("list groups default", func(t *testing.T) {
+		setup(t)
+		ctx, bufs := PatchCLI(context.Background())
+		err := Run(ctx, "groups", "list")
+		assert.NilError(t, err)
+
+		golden.Assert(t, bufs.Stdout.String(), t.Name())
+	})
+
+	t.Run("no truncate", func(t *testing.T) {
+		setup(t)
+		ctx, bufs := PatchCLI(context.Background())
+		err := Run(ctx, "groups", "list", "--no-truncate")
+		assert.NilError(t, err)
+
+		golden.Assert(t, bufs.Stdout.String(), t.Name())
+	})
+
+	t.Run("num users", func(t *testing.T) {
+		setup(t)
+		ctx, bufs := PatchCLI(context.Background())
+		err := Run(ctx, "groups", "list", "--num-users", "2")
+		assert.NilError(t, err)
+
+		golden.Assert(t, bufs.Stdout.String(), t.Name())
+	})
+}
