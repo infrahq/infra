492e17c69fa42c9f5db5a5d1da996d2aaa47a15b
Jeffrey Morgan <jmorganca@gmail.com>
2022-08-04 17:52:26 -0400

    fix(ui): use inherited grants instead of fetching groups

--- ui/lib/admin.js
+++ ui/lib/admin.js
@@ -5,35 +5,15 @@ const INFRA_ADMIN_ROLE = 'admin'
 export function useAdmin() {
   const { data: auth } = useSWR('/api/users/self', { revalidateIfStale: false })
   const { data: { items: grants } = {} } = useSWR(
-    `/api/grants?user=${auth?.id}`,
-    { revalidateIfStale: false }
-  )
-  const { data: { items: groups } = {} } = useSWR(
-    `/api/groups?userID=${auth?.id}`,
+    `/api/grants?user=${auth?.id}&showInherited=1&resource=infra`,
     { revalidateIfStale: false }
   )
 
-  // todo: switch to using /api/grants?inherited=1 instead of
-  // multiple fetches (/api/grants for each group)
-  const { data: groupGrantDatas } = useSWR(
-    () => groups.map(g => `/api/grants?group=${g.id}`) || null,
-    (...urls) => Promise.all(urls.map(url => fetch(url).then(r => r.json()))),
-    { revalidateIfStale: false }
-  )
-
-  const inherited = groupGrantDatas?.map(g => g.items)?.flat()
-
-  const merged = [...(grants || []), ...(inherited || [])]
-  const loading = [
-    auth,
-    grants,
-    groups,
-    groups?.length ? inherited : true,
-  ].some(x => !x)
-  const admin = merged.some(g => g.privilege === INFRA_ADMIN_ROLE)
+  const loading = !auth && !grants
+  const admin = grants?.some(g => g.privilege === INFRA_ADMIN_ROLE)
 
   return {
-    loading,
     admin,
+    loading,
   }
 }
