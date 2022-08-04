ef6b649b2a35079d46cf3d8bb7cc70d4ddb6cf7d
Daniel Nephin <dnephin@infrahq.com>
2022-08-04 18:35:48 -0400

    maintain: remove unused models.Base64
    This type is not used, and it doesn't have test coverage. Likely we will serialize
    base64 values as string or []byte.

--- internal/server/models/types.go
+++ internal/server/models/types.go
@@ -2,33 +2,9 @@ package models
 
 import (
 	"database/sql/driver"
-	"encoding/base64"
 	"fmt"
 	"strings"
 )
-
-type Base64 []byte
-
-func (f Base64) Value() (driver.Value, error) {
-	r := base64.StdEncoding.EncodeToString([]byte(f))
-
-	return r, nil
-}
-
-func (f *Base64) Scan(v interface{}) error {
-	b, err := base64.StdEncoding.DecodeString(string(v.(string)))
-	if err != nil {
-		return fmt.Errorf("base64 decoding field: %w", err)
-	}
-
-	*f = Base64(b)
-
-	return nil
-}
-
-func (f Base64) GormDataType() string {
-	return "text"
-}
 
 type CommaSeparatedStrings []string
 
@@ -50,7 +26,7 @@ func (s *CommaSeparatedStrings) Scan(v interface{}) error {
 		parts = parts[1:]
 	}
 
-	*s = CommaSeparatedStrings(parts)
+	*s = parts
 
 	return nil
 }
