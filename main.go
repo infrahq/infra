46b4ea0553497ca339af38f362ac231386b0484d
kimskimchi <elizabethkim@infrahq.com>
2022-08-10 03:06:33 -0400

    improve: better error message for cert mismatch

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/AlecAivazis/survey/v2/terminal"
 
@@ -20,6 +21,10 @@ func main() {
 			logging.Debugf("user interrupted the process")
 		case errors.As(err, &userErr):
 			fmt.Fprintln(os.Stderr, userErr.Error())
+		case strings.Contains(err.Error(), "x509: certificate signed by unknown authority"):
+			// Cert error is most likely caused by mismatch between the client session and its cached server
+			// eg) server was re-installed, but the client cache was not cleared
+			fmt.Fprintf(os.Stderr, "Session certificate is no longer valid for this server; run 'infra login' to start a new session:\n\n%v\n", err)
 		default:
 			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		}
