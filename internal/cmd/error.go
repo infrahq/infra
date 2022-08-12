a3e4dcd0db602b1c77e4d63ebba3cbdceb82e520
kimskimchi <elizabethkim@infrahq.com>
2022-08-12 13:30:48 -0400

    improve: draft

--- internal/cmd/error.go
+++ internal/cmd/error.go
@@ -1,36 +1,51 @@
 package cmd
 
-import (
-	"fmt"
-	"strings"
-)
-
 // CLI Errors are user facing errors that are formatted.
 // Should be used for communication, rather than a stacktrace.
 type Error struct {
-	// OriginalError is an error that is caused by the system, used for logging/debugging
+	// Short redacted version of OriginalError, required if OriginalError is set
+	Cause string
+
+	// OriginalError is the error that bubbled up, used for logging/debugging
 	// Only set this if you need it to be printed as part of the user facing 'Message'.
 	OriginalError error
 
-	// Message is a human readable error message that user will read on the CLI.
-	// These should be full sentences, rather than a stack trace.
-	// Consider including suggestions to resolve the error.
-	Message string
+	// Human readable message to resolve the error. These should be full sentences.
+	Suggestion string
 }
 
+// Format is one of the three:
+// a) Error: Cause
+//    OriginalError
+//
+//    Suggestion
+//
+// b) Error: Cause
+//    Suggestion
+//
+// c) Suggestion
 func (e Error) Error() string {
-	if e.OriginalError != nil {
-		if len(e.Message) == 0 {
-			return fmt.Sprintf("Error: %v", e.OriginalError)
-		}
-
-		// Strip '.' at the end when message includes the original error
-		return fmt.Sprintf("%s: %v", strings.TrimSuffix(e.Message, "."), e.OriginalError)
+	if e.OriginalError == nil && len(e.Cause) == 0 {
+		return e.Suggestion
 	}
 
-	return e.Message
+	output := "Error: " + e.Cause
+	if e.OriginalError != nil {
+		output += "\n" + e.OriginalError.Error()
+	}
+
+	if len(e.Suggestion) > 0 {
+		output += "\n\n" + e.Suggestion
+	}
+
+	return output
 }
 
 func (e Error) Unwrap() error {
 	return e.OriginalError
+}
+
+var unauthorizedError = Error{
+	Cause:      "missing permissions to run this command",
+	Suggestion: "Please make sure you have the correct grants.",
 }
