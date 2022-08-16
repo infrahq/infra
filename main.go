60719253a6c0738ed57b7d4e9c8daab1f94182a8
kimskimchi <elizabethkim@infrahq.com>
2022-08-16 12:02:03 -0400

    improve: better error message for cert mismatch (#2899)
    * improve: better error message for cert mismatch
    
    * improve: update
    
    * improve: small
    
    * improve: small

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/x509"
 	"errors"
 	"fmt"
 	"os"
@@ -15,11 +16,17 @@ import (
 func main() {
 	if err := cmd.Run(context.Background(), os.Args[1:]...); err != nil {
 		var userErr cmd.Error
+		var unknownAuthErr x509.UnknownAuthorityError
+
 		switch {
 		case errors.Is(err, terminal.InterruptErr):
 			logging.Debugf("user interrupted the process")
 		case errors.As(err, &userErr):
 			fmt.Fprintln(os.Stderr, userErr.Error())
+		case errors.As(err, &unknownAuthErr):
+			// Cert error is most likely caused by mismatch between the client session and its cached server
+			// eg) server was re-installed, but the client cache was not cleared
+			fmt.Fprintf(os.Stderr, "Error: %v\n\nTLS certificate is no longer valid with this server; to see if a new session resolves the issue, run `infra login` again OR `infra logout --clear`\n", err)
 		default:
 			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		}
