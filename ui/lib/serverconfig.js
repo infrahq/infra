0d6a6a9415397c0d441dacf5b68854ad9ddd8987
Steven Soroka <steven@infrahq.com>
2022-08-16 19:11:43 -0400

    fix: api/configuration

--- ui/lib/serverconfig.js
+++ ui/lib/serverconfig.js
@@ -2,7 +2,7 @@ import useSWR from 'swr'
 
 export function useServerConfig() {
   const { data: { isEmailConfigured, isSignupEnabled } = {} } = useSWR(
-    `/api/server-config`,
+    `/api/configuration`,
     {
       revalidateIfStale: false,
     }
