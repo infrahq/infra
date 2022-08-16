a366c6b41f1d8275657abdb8ecba8844dfa97d7f
Steven Soroka <steven@infrahq.com>
2022-08-16 15:36:46 -0400

    fix: test should not hit external url

--- internal/server/config_test.go
+++ internal/server/config_test.go
@@ -635,6 +635,7 @@ func TestLoadConfigWithGroupGrants(t *testing.T) {
 }
 
 func TestLoadConfigPruneConfig(t *testing.T) {
+	t.Skip("do not hit external urls") // this test needs to not hit https://demo.okta.com/.well-known/openid-configuration
 	s := setupServer(t)
 
 	config := Config{
