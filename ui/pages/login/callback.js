9f45d78a40f53925fb0b191047a1f6d10baddeb1
Jeffrey Morgan <jmorganca@gmail.com>
2022-08-18 16:09:02 -0400

    fix: dont re-render login page on sso login

--- ui/pages/login/callback.js
+++ ui/pages/login/callback.js
@@ -5,6 +5,8 @@ import { useSWRConfig } from 'swr'
 export default function Callback() {
   const { mutate } = useSWRConfig()
   const router = useRouter()
+  const { isReady } = router
+  const { code, state } = router.query
 
   useEffect(() => {
     async function login({ providerID, code, redirectURL }) {
@@ -23,28 +25,34 @@ export default function Callback() {
       router.replace('/')
     }
 
-    const urlSearchParams = new URLSearchParams(window.location.search)
-    const params = Object.fromEntries(urlSearchParams.entries())
-
     const providerID = window.localStorage.getItem('providerID')
     const redirectURL = window.localStorage.getItem('redirectURL')
 
-    if (!params.code || !providerID || !redirectURL) {
-      router.replace('/login')
-      return
-    }
-
-    if (params.state === window.localStorage.getItem('state')) {
+    if (
+      state === window.localStorage.getItem('state') &&
+      code &&
+      providerID &&
+      redirectURL
+    ) {
       login({
         providerID,
-        code: params.code,
+        code,
         redirectURL,
       })
       window.localStorage.removeItem('providerID')
       window.localStorage.removeItem('state')
       window.localStorage.removeItem('redirectURL')
     }
-  }, [router, mutate])
+  }, [code, state, mutate, router])
+
+  if (!isReady) {
+    return null
+  }
+
+  if (!state || !code) {
+    router.replace('/login')
+    return null
+  }
 
   return (
     <div className='flex h-full w-full items-center justify-center'>
