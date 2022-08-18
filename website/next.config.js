01ed352cca9f1801633fea9c0491e295e2c22833
Jeffrey Morgan <jmorganca@gmail.com>
2022-08-18 01:30:48 -0400

    fix: add X-Frame-Options headers

--- website/next.config.js
+++ website/next.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+        ],
+      },
+    ]
+  },
   async redirects() {
     return [
       {
