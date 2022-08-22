a8eb2bfe2f754c78cfebe4823f4eea14014058bb
Daniel Nephin <dnephin@infrahq.com>
2022-08-22 16:31:48 -0400

    improve: use runDBTests in org create test

--- internal/server/data/organization_test.go
+++ internal/server/data/organization_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/google/go-cmp/cmp/cmpopts"
+	"gorm.io/gorm"
 	"gotest.tools/v3/assert"
 
 	"github.com/infrahq/infra/internal/server/models"
@@ -14,59 +15,59 @@ import (
 var cmpTimeWithDBPrecision = cmpopts.EquateApproxTime(time.Microsecond)
 
 func TestCreateOrganizationAndSetContext(t *testing.T) {
-	pgsql := postgresDriver(t)
-	db := setupDB(t, pgsql)
+	runDBTests(t, func(t *testing.T, db *gorm.DB) {
 
-	org := &models.Organization{Name: "syndicate", Domain: "syndicate-123"}
+		org := &models.Organization{Name: "syndicate", Domain: "syndicate-123"}
 
-	err := CreateOrganizationAndSetContext(db, org)
-	assert.NilError(t, err)
+		err := CreateOrganizationAndSetContext(db, org)
+		assert.NilError(t, err)
 
-	// db context is set
-	ctxOrg := OrgFromContext(db.Statement.Context)
-	assert.DeepEqual(t, org, ctxOrg, cmpTimeWithDBPrecision)
+		// db context is set
+		ctxOrg := OrgFromContext(db.Statement.Context)
+		assert.DeepEqual(t, org, ctxOrg, cmpTimeWithDBPrecision)
 
-	// org is created
-	readOrg, err := GetOrganization(db, ByID(org.ID))
-	assert.NilError(t, err)
-	assert.DeepEqual(t, org, readOrg, cmpTimeWithDBPrecision)
+		// org is created
+		readOrg, err := GetOrganization(db, ByID(org.ID))
+		assert.NilError(t, err)
+		assert.DeepEqual(t, org, readOrg, cmpTimeWithDBPrecision)
 
-	// infra provider is created
-	orgInfraIDP := InfraProvider(db)
-	assert.NilError(t, err)
+		// infra provider is created
+		orgInfraIDP := InfraProvider(db)
+		assert.NilError(t, err)
 
-	expectedOrgInfraProviderIDP := &models.Provider{
-		Model:              orgInfraIDP.Model, // does not matter
-		OrganizationMember: models.OrganizationMember{OrganizationID: org.ID},
-		Scopes:             models.CommaSeparatedStrings{},
-		Name:               models.InternalInfraProviderName,
-		Kind:               models.ProviderKindInfra,
-		CreatedBy:          models.CreatedBySystem,
-	}
-	assert.DeepEqual(t, orgInfraIDP, expectedOrgInfraProviderIDP, cmpTimeWithDBPrecision)
+		expectedOrgInfraProviderIDP := &models.Provider{
+			Model:              orgInfraIDP.Model, // does not matter
+			OrganizationMember: models.OrganizationMember{OrganizationID: org.ID},
+			Scopes:             models.CommaSeparatedStrings{},
+			Name:               models.InternalInfraProviderName,
+			Kind:               models.ProviderKindInfra,
+			CreatedBy:          models.CreatedBySystem,
+		}
+		assert.DeepEqual(t, orgInfraIDP, expectedOrgInfraProviderIDP, cmpTimeWithDBPrecision)
 
-	// the org connector is created and granted approprite access
-	connector, err := GetIdentity(db, ByName(models.InternalInfraConnectorIdentityName))
-	assert.NilError(t, err)
+		// the org connector is created and granted approprite access
+		connector, err := GetIdentity(db, ByName(models.InternalInfraConnectorIdentityName))
+		assert.NilError(t, err)
 
-	expectedConnector := &models.Identity{
-		Model:              connector.Model,
-		OrganizationMember: models.OrganizationMember{OrganizationID: org.ID},
-		Name:               models.InternalInfraConnectorIdentityName,
-		CreatedBy:          models.CreatedBySystem,
-	}
-	assert.DeepEqual(t, connector, expectedConnector)
+		expectedConnector := &models.Identity{
+			Model:              connector.Model,
+			OrganizationMember: models.OrganizationMember{OrganizationID: org.ID},
+			Name:               models.InternalInfraConnectorIdentityName,
+			CreatedBy:          models.CreatedBySystem,
+		}
+		assert.DeepEqual(t, connector, expectedConnector)
 
-	connectorGrant, err := GetGrant(db, BySubject(connector.PolyID()), ByPrivilege(models.InfraAdminRole), ByResource("infra"))
-	assert.NilError(t, err)
+		connectorGrant, err := GetGrant(db, BySubject(connector.PolyID()), ByPrivilege(models.InfraAdminRole), ByResource("infra"))
+		assert.NilError(t, err)
 
-	expectedConnectorGrant := &models.Grant{
-		Model:              connectorGrant.Model,
-		OrganizationMember: models.OrganizationMember{OrganizationID: org.ID},
-		Subject:            connector.PolyID(),
-		Privilege:          models.InfraAdminRole,
-		Resource:           "infra",
-		CreatedBy:          models.CreatedBySystem,
-	}
-	assert.DeepEqual(t, connectorGrant, expectedConnectorGrant)
+		expectedConnectorGrant := &models.Grant{
+			Model:              connectorGrant.Model,
+			OrganizationMember: models.OrganizationMember{OrganizationID: org.ID},
+			Subject:            connector.PolyID(),
+			Privilege:          models.InfraAdminRole,
+			Resource:           "infra",
+			CreatedBy:          models.CreatedBySystem,
+		}
+		assert.DeepEqual(t, connectorGrant, expectedConnectorGrant)
+	})
 }
