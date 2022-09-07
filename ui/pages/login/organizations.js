8c31cd0b35c75de0c1e5825e2b4128ec2cc4c301
kimskimchi <elizabethkim@infrahq.com>
2022-09-07 11:23:10 -0400

    improve: small

--- ui/pages/login/organizations.js
+++ ui/pages/login/organizations.js
@@ -11,23 +11,21 @@ export default function PreLogin() {
       <h2 className='my-1.5 mb-4 max-w-md text-center text-xs text-gray-400'>
         Choose your organization to login to.
       </h2>
-      {organizations?.length > 0 && (
+      <>
         <>
-          <>
-            {organizations?.map(o => (
-              <a
-                href={`//${o.url}`}
-                key={o.name}
-                className='mt-1 mb-1 w-full rounded-lg border border-violet-300 px-4 py-3 text-center text-2xs text-violet-100 hover:border-violet-100 disabled:pointer-events-none disabled:opacity-30'
-              >
-                {o.url}
-                <br />
-                {o.user}
-              </a>
-            ))}
-          </>
+          {organizations?.map(o => (
+            <a
+              href={`//${o.url}`}
+              key={o.name}
+              className='mt-1 mb-1 w-full rounded-lg border border-violet-300 px-4 py-3 text-center text-2xs text-violet-100 hover:border-violet-100 disabled:pointer-events-none disabled:opacity-30'
+            >
+              {o.url}
+              <br />
+              {o.user}
+            </a>
+          ))}
         </>
-      )}
+      </>
     </>
   )
 }
