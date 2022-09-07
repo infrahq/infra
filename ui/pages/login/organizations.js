1cb77c5a4badfe3c2b7f3dbc2403d1a1827b770a
kimskimchi <elizabethkim@infrahq.com>
2022-09-07 15:52:36 -0400

    improve: small

--- ui/pages/login/organizations.js
+++ ui/pages/login/organizations.js
@@ -15,13 +15,15 @@ export default function Organizations() {
       </h2>
       <>
         {organizations?.map(o => (
-          <Link href={`//${o.url}`} key={o.name}>
-            <a className='mt-1 mb-1 w-full rounded-lg border border-violet-300 px-4 py-3 text-center text-2xs text-violet-100 hover:border-violet-100 disabled:pointer-events-none disabled:opacity-30'>
-              {o.url}
-              <br />
-              {o.user}
-            </a>
-          </Link>
+          <a
+            href={`//${o.url}`}
+            key={o.name}
+            className='mt-1 mb-1 w-full rounded-lg border border-violet-300 px-4 py-3 text-center text-2xs text-violet-100 hover:border-violet-100 disabled:pointer-events-none disabled:opacity-30'
+          >
+            {o.url}
+            <br />
+            {o.user}
+          </a>
         ))}
       </>
     </>
