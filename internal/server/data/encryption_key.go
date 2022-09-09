1b1a6e848319cbc1652113bc75278b30c0399d0e
Daniel Nephin <dnephin@infrahq.com>
2022-09-09 11:20:03 -0400

    improve: add table methods for encryption keys

--- internal/server/data/encryption_key.go
+++ internal/server/data/encryption_key.go
@@ -8,6 +8,24 @@ import (
 	"github.com/infrahq/infra/internal/server/models"
 )
 
+type encryptionKeysTable models.EncryptionKey
+
+func (e encryptionKeysTable) Table() string {
+	return "encryption_keys"
+}
+
+func (e encryptionKeysTable) Columns() []string {
+	return []string{"algorithm", "created_at", "deleted_at", "encrypted", "id", "key_id", "name", "root_key_id", "updated_at"}
+}
+
+func (e encryptionKeysTable) Values() []any {
+	return []any{e.Algorithm, e.CreatedAt, e.DeletedAt, e.Encrypted, e.ID, e.KeyID, e.Name, e.RootKeyID, e.UpdatedAt}
+}
+
+func (e *encryptionKeysTable) ScanFields() []any {
+	return []any{&e.Algorithm, &e.CreatedAt, &e.DeletedAt, &e.Encrypted, &e.ID, &e.KeyID, &e.Name, &e.RootKeyID, &e.UpdatedAt}
+}
+
 func CreateEncryptionKey(db GormTxn, key *models.EncryptionKey) (*models.EncryptionKey, error) {
 	if key.KeyID == 0 {
 		// not a security issue; just an identifier
