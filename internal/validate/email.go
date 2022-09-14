841e29b03c36ba97414bc8f02c76507299b97db7
Steven Soroka <steven@infrahq.com>
2022-09-14 15:50:37 -0400

    fix: better errors on bad email

--- internal/validate/email.go
+++ internal/validate/email.go
@@ -3,6 +3,7 @@ package validate
 import (
 	"fmt"
 	"net/mail"
+	"strings"
 
 	"github.com/getkin/kin-openapi/openapi3"
 )
@@ -27,6 +28,10 @@ func (e email) Validate() *Failure {
 	}
 	if addr.Name != "" {
 		return fail(e.name, fmt.Sprintf("email address must not contain display name %q", addr.Name))
+	}
+	// domain must contain at least one dot. ParseAddress already checks that it's not leading or trailing
+	if !strings.Contains(strings.Split(addr.Address, "@")[1], ".") {
+		return fail(e.name, "email address must contain at least one '.' character")
 	}
 	return nil
 }
