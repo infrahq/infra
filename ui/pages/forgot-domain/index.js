00775cf4b263ef34d1c571e3326caaa3a9563d8e
Jeffrey Morgan <jmorganca@gmail.com>
2022-09-15 11:08:28 -0700

    fix: tweak find org page

--- ui/pages/forgot-domain/index.js
+++ ui/pages/forgot-domain/index.js
@@ -37,8 +37,9 @@ export default function ForgotDomain() {
       <h1 className='text-base font-bold leading-snug'>
         Find your organization
       </h1>
-      <h2 className='my-1.5 mb-4 max-w-md text-center text-xs text-gray-500'>
-        Choose an organization to log in to.
+      <h2 className='my-2 text-center text-sm text-gray-500'>
+        Enter your email, and we&apos;ll send you a list of organizations you
+        are a member of.
       </h2>
       {submitted ? (
         <p className='my-3 flex max-w-[260px] flex-1 flex-col items-center justify-center text-center text-xs text-gray-600'>
@@ -47,46 +48,40 @@ export default function ForgotDomain() {
           your organizations.
         </p>
       ) : (
-        <>
-          <h2 className='my-2 text-center text-sm text-gray-500'>
-            Enter your email, and we&apos;ll send you a list of organizations
-            you are a member of.
-          </h2>
-          <form
-            onSubmit={onSubmit}
-            className='relative flex w-full max-w-sm flex-1 flex-col justify-center'
-          >
-            <div className='my-2 w-full'>
-              <label
-                htmlFor='password'
-                className='text-2xs font-medium text-gray-700'
-              >
-                Email
-              </label>
-              <input
-                required
-                autoFocus
-                id='name'
-                type='email'
-                onChange={e => {
-                  setEmail(e.target.value)
-                  setError('')
-                }}
-                className={`mt-1 block w-full rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm ${
-                  error ? 'border-red-500' : 'border-gray-300'
-                }`}
-              />
-            </div>
-            <button className='mt-4 mb-2 flex w-full cursor-pointer justify-center rounded-md border border-transparent bg-blue-500 py-2 px-4 font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:text-sm'>
-              Find your organization
-            </button>
-            {error && (
-              <p className='absolute -bottom-3.5 mx-auto w-full text-center text-2xs text-red-500'>
-                {error}
-              </p>
-            )}
-          </form>
-        </>
+        <form
+          onSubmit={onSubmit}
+          className='relative flex w-full max-w-sm flex-1 flex-col justify-center'
+        >
+          <div className='my-2 w-full'>
+            <label
+              htmlFor='password'
+              className='text-2xs font-medium text-gray-700'
+            >
+              Email
+            </label>
+            <input
+              required
+              autoFocus
+              id='name'
+              type='email'
+              onChange={e => {
+                setEmail(e.target.value)
+                setError('')
+              }}
+              className={`mt-1 block w-full rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm ${
+                error ? 'border-red-500' : 'border-gray-300'
+              }`}
+            />
+          </div>
+          <button className='mt-4 mb-2 flex w-full cursor-pointer justify-center rounded-md border border-transparent bg-blue-500 py-2 px-4 font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:text-sm'>
+            Find your organization
+          </button>
+          {error && (
+            <p className='absolute -bottom-3.5 mx-auto w-full text-center text-2xs text-red-500'>
+              {error}
+            </p>
+          )}
+        </form>
       )}
     </div>
   )
