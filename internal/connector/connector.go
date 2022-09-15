67e68e11783ac268699a305cf3b9d9c17daec000
Daniel Nephin <dnephin@infrahq.com>
2022-09-15 14:19:28 -0400

    maintain: use string or file for connector ca config
    This would allow someone to set these values from environment variable instead of it always
    being from a file.

--- internal/connector/connector.go
+++ internal/connector/connector.go
@@ -11,7 +11,6 @@ import (
 	"net"
 	"net/http"
 	"net/http/httputil"
-	"os"
 	"strconv"
 	"strings"
 	"sync"
@@ -36,8 +35,8 @@ import (
 type Options struct {
 	Server ServerOptions
 	Name   string
-	CACert string
-	CAKey  string
+	CACert types.StringOrFile
+	CAKey  types.StringOrFile
 
 	Addr ListenerOptions
 }
@@ -87,17 +86,7 @@ func Run(ctx context.Context, options Options) error {
 		options.Name = autoname
 	}
 
-	caCertPEM, err := os.ReadFile(options.CACert)
-	if err != nil {
-		return err
-	}
-
-	caKeyPEM, err := os.ReadFile(options.CAKey)
-	if err != nil {
-		return err
-	}
-
-	certCache := NewCertCache(caCertPEM, caKeyPEM)
+	certCache := NewCertCache([]byte(options.CACert), []byte(options.CAKey))
 
 	// Generate TLS certificates on the fly for clients
 	// GenerateCertificate caches certificates
@@ -183,7 +172,7 @@ func Run(ctx context.Context, options Options) error {
 	}
 
 	// TODO: make polling time configurable
-	repeat.Start(ctx, 30*time.Second, syncWithServer(k8s, client, destination, certCache, caCertPEM))
+	repeat.Start(ctx, 30*time.Second, syncWithServer(k8s, client, destination, certCache, []byte(options.CACert)))
 
 	ginutil.SetMode()
 	router := gin.New()
