b3ff529d264c3a5c8d90be77b5914efc1603a4fb
Eva Ho <eva.ho@infrahq.com>
2022-09-19 21:58:20 -0400

    fix: build

--- ui/pages/providers/add.js
+++ ui/pages/providers/add.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
-import Link from 'next/link'
 import { useSWRConfig } from 'swr'
 import { InformationCircleIcon } from '@heroicons/react/outline'
 
