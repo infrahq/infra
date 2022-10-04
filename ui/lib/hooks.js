991e14bf17de5c6230b43798d3088b3fa8d218f0
Jeffrey Morgan <jmorganca@gmail.com>
2022-10-04 16:23:35 -0400

    fix: minor flashes of content when logging in

--- ui/lib/hooks.js
+++ ui/lib/hooks.js
@@ -4,7 +4,7 @@ import useSWR from 'swr'
 const INFRA_ADMIN_ROLE = 'admin'
 
 export function useUser({ redirectTo, redirectIfFound } = {}) {
-  const { data: user, error } = useSWR('/api/users/self')
+  const { data: user, error, isValidating } = useSWR('/api/users/self')
   const { data: { items: grants } = {}, grantsError } = useSWR(() =>
     user
       ? `/api/grants?user=${user?.id}&showInherited=1&resource=infra&limit=1000`
@@ -23,7 +23,10 @@ export function useUser({ redirectTo, redirectIfFound } = {}) {
   }
 
   // Redirect
-  if ((redirectTo && !redirectIfFound && !user) || (redirectIfFound && user)) {
+  if (
+    !isValidating &&
+    ((redirectTo && !redirectIfFound && !user) || (redirectIfFound && user))
+  ) {
     router.replace(redirectTo)
     return { loading: true }
   }
@@ -36,6 +39,8 @@ export function useUser({ redirectTo, redirectIfFound } = {}) {
       await fetch('/api/logout', {
         method: 'POST',
       })
+
+      // Do a "hard" redirect in order to clear the swr cache of all data
       window.location = '/login'
     },
   }
