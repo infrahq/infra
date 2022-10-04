a4fada561f4dcbfc2170304ffdb77b0700e780fb
Jeffrey Morgan <251292+jmorganca@users.noreply.github.com>
2022-10-04 16:26:15 -0400

    fix: minor flashes of content when logging in (#3389)

--- ui/pages/index.js
+++ ui/pages/index.js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 import { useUser } from '../lib/hooks'
 
@@ -6,12 +7,12 @@ export default function Index() {
   const { loading } = useUser({ redirectTo: '/login' })
   const router = useRouter()
 
+  useEffect(() => {
+    router.replace('/destinations')
+  }, [router])
+
   if (loading) {
     return null
-  }
-
-  if (router.isReady) {
-    router.replace('/destinations')
   }
 
   return null
