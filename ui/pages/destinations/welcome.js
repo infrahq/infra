1bbd9b6e256dd94dfcfb4bfa8198a0eb9fe0b2d2
Patrick Devine <patrick@infrahq.com>
2022-10-12 19:21:28 -0700

    fix: formatting

--- ui/pages/destinations/welcome.js
+++ ui/pages/destinations/welcome.js
@@ -58,108 +58,111 @@ export default function DestinationsAdd() {
         {/* Name form */}
 
         {/* Install CLI + sign in*/}
-          <div className='mb-2'>
-            <h3 className='text-base font-medium leading-6 text-gray-900'>
-              1.
-              <a
-                href='https://infrahq.com/docs/start/install-infra-cli'
-                className='inline-flex items-center underline'
-                target='_blank'
-                rel='noreferrer'
-              >
-                Download the CLI <ExternalLinkIcon className='ml-0.5 h-3 w-3' /> </a> and sign in to your Infra account
-            </h3>
-          </div>
-          <div className='group relative my-4 flex'>
-            <pre className='w-full overflow-auto rounded-lg bg-gray-50 px-5 py-4 text-xs leading-normal text-gray-800'>
-              {command}
-            </pre>
-            <button
-              className={`absolute right-2 top-2 rounded-md border border-black/10 bg-white px-2 py-2 text-black/40 backdrop-blur-xl hover:text-black/70`}
-              onClick={() => {
-                copy(command)
-                setCommandCopied(true)
-                setTimeout(() => setCommandCopied(false), 2000)
-              }}
+        <div className='mb-2'>
+          <h3 className='text-base font-medium leading-6 text-gray-900'>
+            1.
+            <a
+              href='https://infrahq.com/docs/start/install-infra-cli'
+              className='inline-flex items-center underline'
+              target='_blank'
+              rel='noreferrer'
             >
-              {commandCopied ? (
-                <CheckIcon className='h-4 w-4 text-green-500' />
-              ) : (
-                <DuplicateIcon className='h-4 w-4' />
-              )}
-            </button>
-          </div>
+              Download the CLI <ExternalLinkIcon className='ml-0.5 h-3 w-3' />{' '}
+            </a>{' '}
+            and sign in to your Infra account
+          </h3>
+        </div>
+        <div className='group relative my-4 flex'>
+          <pre className='w-full overflow-auto rounded-lg bg-gray-50 px-5 py-4 text-xs leading-normal text-gray-800'>
+            {command}
+          </pre>
+          <button
+            className={`absolute right-2 top-2 rounded-md border border-black/10 bg-white px-2 py-2 text-black/40 backdrop-blur-xl hover:text-black/70`}
+            onClick={() => {
+              copy(command)
+              setCommandCopied(true)
+              setTimeout(() => setCommandCopied(false), 2000)
+            }}
+          >
+            {commandCopied ? (
+              <CheckIcon className='h-4 w-4 text-green-500' />
+            ) : (
+              <DuplicateIcon className='h-4 w-4' />
+            )}
+          </button>
+        </div>
 
         {/* Use infra */}
-          <div className='mb-2'>
-            <h3 className='text-base font-medium leading-6 text-gray-900'>
-              2. Switch to your infra managed cluster
-            </h3>
-          </div>
-          <div className='group relative my-4 flex'>
-            <pre className='w-full overflow-auto rounded-lg bg-gray-50 px-5 py-4 text-xs leading-normal text-gray-800'>
-              {useCommand}
-            </pre>
-            <button
-              className={`absolute right-2 top-2 rounded-md border border-black/10 bg-white px-2 py-2 text-black/40 backdrop-blur-xl hover:text-black/70`}
-              onClick={() => {
-                copy(useCommand)
-                setUseCommandCopied(true)
-                setTimeout(() => setUseCommandCopied(false), 2000)
-              }}
-            >
-              {useCommandCopied ? (
-                <CheckIcon className='h-4 w-4 text-green-500' />
-              ) : (
-                <DuplicateIcon className='h-4 w-4' />
-              )}
-            </button>
-          </div>
+        <div className='mb-2'>
+          <h3 className='text-base font-medium leading-6 text-gray-900'>
+            2. Switch to your infra managed cluster
+          </h3>
+        </div>
+        <div className='group relative my-4 flex'>
+          <pre className='w-full overflow-auto rounded-lg bg-gray-50 px-5 py-4 text-xs leading-normal text-gray-800'>
+            {useCommand}
+          </pre>
+          <button
+            className={`absolute right-2 top-2 rounded-md border border-black/10 bg-white px-2 py-2 text-black/40 backdrop-blur-xl hover:text-black/70`}
+            onClick={() => {
+              copy(useCommand)
+              setUseCommandCopied(true)
+              setTimeout(() => setUseCommandCopied(false), 2000)
+            }}
+          >
+            {useCommandCopied ? (
+              <CheckIcon className='h-4 w-4 text-green-500' />
+            ) : (
+              <DuplicateIcon className='h-4 w-4' />
+            )}
+          </button>
+        </div>
 
         {/* Use kubectl */}
-          <div className='mb-2'>
-            <h3 className='text-base font-medium leading-6 text-gray-900'>
-              3. Use your kubernetes infrastructure
-            </h3>
-            <p className='mt-1 text-sm text-gray-500'>
-              If you have never used kubernetes before, check out&nbsp;
-              <a
-                href='https://kubernetes.io/docs/tutorials/kubernetes-basics/'
-                className='inline-flex items-center underline'
-                target='_blank'
-                rel='noreferrer'
-              >
-                this primer <ExternalLinkIcon className='ml-0.5 h-3 w-3' /> </a> to learn more about the basics
-
-            </p>
-          </div>
-          <div className='group relative my-4 flex'>
-            <pre className='w-full overflow-auto rounded-lg bg-gray-50 px-5 py-4 text-xs leading-normal text-gray-800'>
-              {kubeCommand}
-            </pre>
-            <button
-              className={`absolute right-2 top-2 rounded-md border border-black/10 bg-white px-2 py-2 text-black/40 backdrop-blur-xl hover:text-black/70`}
-              onClick={() => {
-                copy(kubeCommand)
-                setKubeCommandCopied(true)
-                setTimeout(() => setKubeCommandCopied(false), 2000)
-              }}
+        <div className='mb-2'>
+          <h3 className='text-base font-medium leading-6 text-gray-900'>
+            3. Use your kubernetes infrastructure
+          </h3>
+          <p className='mt-1 text-sm text-gray-500'>
+            If you have never used kubernetes before, check out&nbsp;
+            <a
+              href='https://kubernetes.io/docs/tutorials/kubernetes-basics/'
+              className='inline-flex items-center underline'
+              target='_blank'
+              rel='noreferrer'
             >
-              {kubeCommandCopied ? (
-                <CheckIcon className='h-4 w-4 text-green-500' />
-              ) : (
-                <DuplicateIcon className='h-4 w-4' />
-              )}
-            </button>
-          </div>
+              this primer <ExternalLinkIcon className='ml-0.5 h-3 w-3' />{' '}
+            </a>{' '}
+            to learn more about the basics
+          </p>
+        </div>
+        <div className='group relative my-4 flex'>
+          <pre className='w-full overflow-auto rounded-lg bg-gray-50 px-5 py-4 text-xs leading-normal text-gray-800'>
+            {kubeCommand}
+          </pre>
+          <button
+            className={`absolute right-2 top-2 rounded-md border border-black/10 bg-white px-2 py-2 text-black/40 backdrop-blur-xl hover:text-black/70`}
+            onClick={() => {
+              copy(kubeCommand)
+              setKubeCommandCopied(true)
+              setTimeout(() => setKubeCommandCopied(false), 2000)
+            }}
+          >
+            {kubeCommandCopied ? (
+              <CheckIcon className='h-4 w-4 text-green-500' />
+            ) : (
+              <DuplicateIcon className='h-4 w-4' />
+            )}
+          </button>
+        </div>
 
         {/* Finish */}
-          <div className='flex items-center justify-end space-x-3 pt-5 pb-3'>
-            <Link href='/destinations'>
-              <a className='flex-none items-center self-center rounded-md border border-transparent bg-black px-4 py-2 text-2xs font-medium text-white shadow-sm hover:bg-gray-800'>
-                Finish
-              </a>
-            </Link>
+        <div className='flex items-center justify-end space-x-3 pt-5 pb-3'>
+          <Link href='/destinations'>
+            <a className='flex-none items-center self-center rounded-md border border-transparent bg-black px-4 py-2 text-2xs font-medium text-white shadow-sm hover:bg-gray-800'>
+              Finish
+            </a>
+          </Link>
         </div>
       </div>
     </div>
