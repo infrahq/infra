64a8c084d26d6f7ff275508b14ee57e364d99f89
Steven Soroka <steven@infrahq.com>
2022-10-18 11:40:05 -0400

    fix: wording
    Co-authored-by: Bruce MacDonald <brucewmacdonald@gmail.com>

--- api/deviceflow.go
+++ api/deviceflow.go
@@ -9,7 +9,7 @@ type StartDeviceFlowRequest struct {
 }
 
 type DeviceFlowResponse struct {
-	DeviceCode          string `json:"deviceCode" example:"NGU4QWFiNjQ5YmQwNG3YTdmZMEyNzQ3YzQ1YSA" note:"a long string the device will use to exchange for an access token"`
+	DeviceCode          string `json:"deviceCode" example:"NGU4QWFiNjQ5YmQwNG3YTdmZMEyNzQ3YzQ1YSA" note:"a code that a device will use to exchange for an access key after device login is approved"`
 	VerificationURI     string `json:"verificationURI" example:"https://infrahq.com/device" note:"This is the URL the user needs to enter into their browser to start logging in"`
 	UserCode            string `json:"userCode" example:"BDSD-HQMK" note:"This is the text the user will enter at the URL above"`
 	ExpiresInSeconds    int16  `json:"expiresIn" example:"1800" note:"The number of seconds that this set of values is valid"`
