de85ae2b2178db6c65feb7e3b5a6472a761fc062
Jeffrey Morgan <251292+jmorganca@users.noreply.github.com>
2022-10-21 10:12:53 -0400

    fix: color style for connection status (#3487)

--- ui/pages/destinations/[id]/index.js
+++ ui/pages/destinations/[id]/index.js
@@ -637,7 +637,7 @@ export default function DestinationDetail() {
           : 'Connected'
         : 'Disconnected',
       style: destination?.connected
-        ? destination?.connection.url !== ''
+        ? destination?.connection.url === ''
           ? 'bg-yellow-100 text-yellow-800'
           : 'bg-green-100 text-green-800'
         : 'bg-gray-100 text-gray-800',
