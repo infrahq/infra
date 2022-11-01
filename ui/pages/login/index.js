ca2ef471dd52ca38e7e282bdf105d5a515a6b7a3
Bruce MacDonald <brucewmacdonald@gmail.com>
2022-11-01 09:13:42 -0300

    fix: update password after one-time password (#3524)
    - update password requires old password
    - make password update a component to pass old password from login

--- ui/pages/login/index.js
+++ ui/pages/login/index.js
@@ -10,6 +10,7 @@ import { providers as providersList } from '../../lib/providers'
 import { useServerConfig } from '../../lib/serverconfig'
 
 import LoginLayout from '../../components/layouts/login'
+import UpdatePassword from '../../components/update-password'
 
 function oidcLogin({ id, clientID, authURL, scopes }, next) {
   window.localStorage.setItem('providerID', id)
@@ -117,6 +118,7 @@ export default function Login() {
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
   const [errors, setErrors] = useState({})
+  const [updatePasswordForUser, setUpdatePasswordForUser] = useState('')
   const { isEmailConfigured } = useServerConfig()
   const { login } = useUser()
 
@@ -132,11 +134,7 @@ export default function Login() {
       })
 
       if (data.passwordUpdateRequired) {
-        router.replace({
-          pathname: '/login/finish',
-          query: next ? { user: data.userID, next } : { user: data.userID },
-        })
-
+        setUpdatePasswordForUser(data.userID)
         return false
       }
 
@@ -169,82 +167,93 @@ export default function Login() {
       <h1 className='mt-4 font-display text-2xl font-semibold leading-snug'>
         Log in
       </h1>
-      <h2 className='my-2 text-center text-sm text-gray-500'>
-        Welcome back to Infra
-      </h2>
-      {providers?.length > 0 && (
+      {updatePasswordForUser !== '' ? (
+        <UpdatePassword oldPassword={password} user={updatePasswordForUser} />
+      ) : (
         <>
-          <Providers providers={providers || []} />
-          <div className='relative mt-6 mb-2 w-full'>
-            <div
-              className='absolute inset-0 flex items-center'
-              aria-hidden='true'
-            >
-              <div className='w-full border-t border-gray-200' />
+          <h2 className='my-2 text-center text-sm text-gray-500'>
+            Welcome back to Infra
+          </h2>
+          {providers?.length > 0 && (
+            <>
+              <Providers providers={providers || []} />
+              <div className='relative mt-6 mb-2 w-full'>
+                <div
+                  className='absolute inset-0 flex items-center'
+                  aria-hidden='true'
+                >
+                  <div className='w-full border-t border-gray-200' />
+                </div>
+                <div className='relative flex justify-center text-sm'>
+                  <span className='bg-white px-2 text-2xs text-gray-400'>
+                    OR
+                  </span>
+                </div>
+              </div>
+            </>
+          )}
+          <form onSubmit={onSubmit} className='relative flex w-full flex-col'>
+            <div className='my-2 w-full'>
+              <label
+                htmlFor='name'
+                className='text-2xs font-medium text-gray-700'
+              >
+                Email
+              </label>
+              <input
+                required
+                autoFocus
+                id='name'
+                type='email'
+                onChange={e => {
+                  setName(e.target.value)
+                  setErrors({})
+                  setError('')
+                }}
+                className={`mt-1 block w-full rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm ${
+                  errors.name ? 'border-red-500' : 'border-gray-300'
+                }`}
+              />
+              {errors.name && (
+                <p className='my-1 text-xs text-red-500'>{errors.name}</p>
+              )}
             </div>
-            <div className='relative flex justify-center text-sm'>
-              <span className='bg-white px-2 text-2xs text-gray-400'>OR</span>
+            <div className='my-2 w-full'>
+              <label
+                htmlFor='password'
+                className='text-2xs font-medium text-gray-700'
+              >
+                Password
+              </label>
+              <input
+                required
+                id='password'
+                type='password'
+                data-testid='form-field-password'
+                onChange={e => {
+                  setPassword(e.target.value)
+                  setErrors({})
+                  setError('')
+                }}
+                className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm'
+              />
             </div>
-          </div>
+            {isEmailConfigured && (
+              <div className='mt-4 flex items-center justify-end text-sm'>
+                <Link href='/password-reset'>
+                  <a className='font-medium text-blue-600 hover:text-blue-500'>
+                    Forgot your password?
+                  </a>
+                </Link>
+              </div>
+            )}
+            <button className='mt-4 mb-2 flex w-full cursor-pointer justify-center rounded-md border border-transparent bg-blue-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'>
+              Log in
+            </button>
+            {error && <p className='my-1 text-xs text-red-500'>{error}</p>}
+          </form>
         </>
       )}
-      <form onSubmit={onSubmit} className='relative flex w-full flex-col'>
-        <div className='my-2 w-full'>
-          <label htmlFor='name' className='text-2xs font-medium text-gray-700'>
-            Email
-          </label>
-          <input
-            required
-            autoFocus
-            id='name'
-            type='email'
-            onChange={e => {
-              setName(e.target.value)
-              setErrors({})
-              setError('')
-            }}
-            className={`mt-1 block w-full rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm ${
-              errors.name ? 'border-red-500' : 'border-gray-300'
-            }`}
-          />
-          {errors.name && (
-            <p className='my-1 text-xs text-red-500'>{errors.name}</p>
-          )}
-        </div>
-        <div className='my-2 w-full'>
-          <label
-            htmlFor='password'
-            className='text-2xs font-medium text-gray-700'
-          >
-            Password
-          </label>
-          <input
-            required
-            id='password'
-            type='password'
-            data-testid='form-field-password'
-            onChange={e => {
-              setPassword(e.target.value)
-              setErrors({})
-              setError('')
-            }}
-            className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm'
-          />
-        </div>
-        {isEmailConfigured && (
-          <div className='mt-4 flex items-center justify-end text-sm'>
-            <Link href='/password-reset'>
-              <a className='font-medium text-blue-600 hover:text-blue-500'>
-                Forgot your password?
-              </a>
-            </Link>
-          </div>
-        )}
-        <button className='mt-4 mb-2 flex w-full cursor-pointer justify-center rounded-md border border-transparent bg-blue-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'>
-          Log in
-        </button>
-        {error && <p className='my-1 text-xs text-red-500'>{error}</p>}
-      </form>
     </div>
   )
 }
