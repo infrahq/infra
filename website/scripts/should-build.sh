12c88d05dd58b67ccae373479c232aacdac868fe
Jeffrey Morgan <jmorganca@gmail.com>
2022-11-23 19:45:36 -0500

    Move website and docs to a separate repo

--- website/scripts/should-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "$VERCEL_ENV" = "production" ] ; then
-  # Proceed with the build
-  exit 1
-else
-  # only build if website or docs files have changes
-  git diff --quiet HEAD^ HEAD ../docs .
-fi
