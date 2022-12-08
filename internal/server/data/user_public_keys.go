a3f7816d2b08ddf9f9c3b3b569068da10315729f
Daniel Nephin <dnephin@infrahq.com>
2022-12-08 18:35:46 -0500

    Remove a users public keys when the user is deleted
    Also make the PublicKey field required

--- internal/server/data/user_public_keys.go
+++ internal/server/data/user_public_keys.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/infrahq/infra/internal/server/data/querybuilder"
 	"github.com/infrahq/infra/internal/server/models"
@@ -26,7 +27,7 @@ func (t *userPublicKeysTable) ScanFields() []any {
 	return []any{&t.CreatedAt, &t.DeletedAt, &t.ExpiresAt, &t.Fingerprint, &t.ID, &t.KeyType, &t.Name, &t.PublicKey, &t.UpdatedAt, &t.UserID}
 }
 
-func userPublicKeys(tx ReadTxn, userID uid.ID) ([]models.UserPublicKey, error) {
+func listUserPublicKeys(tx ReadTxn, userID uid.ID) ([]models.UserPublicKey, error) {
 	table := userPublicKeysTable{}
 	query := querybuilder.New("SELECT")
 	query.B(columnsForSelect(table))
@@ -51,6 +52,14 @@ func AddUserPublicKey(tx WriteTxn, key *models.UserPublicKey) error {
 		return fmt.Errorf("fingerprint is required")
 	case key.KeyType == "":
 		return fmt.Errorf("key type is required")
+	case key.PublicKey == "":
+		return fmt.Errorf("public key is required")
 	}
 	return insert(tx, (*userPublicKeysTable)(key))
+}
+
+func deleteUserPublicKeys(tx WriteTxn, userID uid.ID) error {
+	stmt := "UPDATE user_public_keys SET deleted_at = ? WHERE user_id = ?"
+	_, err := tx.Exec(stmt, time.Now(), userID)
+	return handleError(err)
 }
