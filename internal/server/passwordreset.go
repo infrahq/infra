85413d78e76991ee6ea95d8b4d3dda031ab81434
Daniel Nephin <dnephin@infrahq.com>
2022-12-09 14:36:29 -0500

    Remove one password reset function from access
    Also move the full test coverage to the server package.
    
    The second password reset function can't be moved yet because it calls
    an unexported updateCredential.

--- internal/server/passwordreset.go
+++ internal/server/passwordreset.go
@@ -10,34 +10,42 @@ import (
 	"github.com/infrahq/infra/api"
 	"github.com/infrahq/infra/internal"
 	"github.com/infrahq/infra/internal/access"
+	"github.com/infrahq/infra/internal/server/data"
 	"github.com/infrahq/infra/internal/server/email"
 	"github.com/infrahq/infra/internal/server/redis"
 )
 
 func (a *API) RequestPasswordReset(c *gin.Context, r *api.PasswordResetRequest) (*api.EmptyResponse, error) {
+	rCtx := getRequestContext(c)
+	// no authorization required
 	if err := redis.NewLimiter(a.server.redis).RateOK(r.Email, 10); err != nil {
 		return nil, err
 	}
 
-	token, user, err := access.PasswordResetRequest(c, r.Email, 15*time.Minute)
-	if err != nil {
-		if errors.Is(err, internal.ErrNotFound) {
-			return nil, nil // This is okay. we don't notify the user if we failed to find the email.
-		}
+	user, err := data.GetIdentity(rCtx.DBTxn, data.GetIdentityOptions{ByName: r.Email})
+	switch {
+	case errors.Is(err, internal.ErrNotFound):
+		return nil, nil // This is okay. we don't notify the user if we failed to find the email.
+	case err != nil:
 		return nil, err
 	}
 
-	org := access.GetRequestContext(c).Authenticated.Organization
+	_, err = data.GetCredentialByUserID(rCtx.DBTxn, user.ID)
+	if err != nil {
+		// if credential is not found, the user cannot reset their password.
+		return nil, err
+	}
 
-	// send email
+	token, err := data.CreatePasswordResetToken(rCtx.DBTxn, user.ID, 15*time.Minute)
+	if err != nil {
+		return nil, err
+	}
+
+	org := rCtx.Authenticated.Organization
 	err = email.SendPasswordResetEmail("", r.Email, email.PasswordResetData{
 		Link: wrapLinkWithVerification(fmt.Sprintf("https://%s/password-reset?token=%s", org.Domain, token), org.Domain, user.VerificationToken),
 	})
-	if err != nil {
-		return nil, err
-	}
-
-	return nil, nil
+	return nil, err
 }
 
 func (a *API) VerifiedPasswordReset(c *gin.Context, r *api.VerifiedResetPasswordRequest) (*api.LoginResponse, error) {
