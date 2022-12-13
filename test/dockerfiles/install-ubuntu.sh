790a085843e923796f0a920ffcbf84bcb937adb9
Daniel Nephin <dnephin@infrahq.com>
2022-12-13 13:57:47 -0500

    Add ubuntu destination

--- test/dockerfiles/install-ubuntu.sh
+++ test/dockerfiles/install-ubuntu.sh
@@ -2,10 +2,17 @@
 
 set -eu -o pipefail
 
+echo "Running Infra connector install on Ubuntu"
+
+PACKAGE_PATH=/work/dist/infra_0.0.0_amd64.deb
+DESTINATION_ADDR="$(hostname -I)"
+INFRA_SERVER_URL=test-server-1
+INFRA_ACCESS_KEY= #TODO: 
+
 
 # step=install-package
 dpkg -i "${PACKAGE_PATH}"
-apt-get update && apt-get install openssh-server
+apt-get update && apt-get install --no-install-recommends -y openssh-server
 mkdir -p /run/sshd
 
 
@@ -22,11 +29,19 @@ EOF
 # step=write-connector.yaml
 cat << EOF > /etc/infra/connector.yaml
 kind: ssh
-name: testing-ssh
+name: ubuntu
 endpointAddr: "$DESTINATION_ADDR"
 server:
   url: "$INFRA_SERVER_URL"
   accessKey: "$INFRA_ACCESS_KEY"
 EOF
 
-# TODO: exec s6
+
+echo "Starting infra service"
+
+systemctl start infra
+
+
+echo "Starting sshd service"
+
+systemctl start ssh
