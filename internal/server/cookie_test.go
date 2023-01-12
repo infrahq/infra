9bb01cd1118aead48310735b86435114e2bb06a9
Bruce MacDonald <brucewmacdonald@gmail.com>
2023-01-12 16:08:23 -0400

    fix: remove non-secure cookies on logout (#4006)

--- internal/server/cookie_test.go
+++ internal/server/cookie_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -16,6 +17,7 @@ func TestResendAuthCookie(t *testing.T) {
 	req := &http.Request{
 		Host:   fmt.Sprintf("dev.%s", baseDomain),
 		Header: make(http.Header),
+		TLS:    &tls.ConnectionState{}, // if this isnt nil the request is considered secure
 	}
 	maxAge := int(time.Until(time.Now().Add(5 * time.Second)).Seconds())
 	req.AddCookie(&http.Cookie{
@@ -39,10 +41,32 @@ func TestResendAuthCookie(t *testing.T) {
 	cookies := resp.Header()["Set-Cookie"]
 
 	matched, err := regexp.MatchString(
-		"auth=aaa; Path=/; Domain=dev.example.com; Max-Age=\\d\\d; HttpOnly; SameSite=Strict",
+		"auth=aaa; Path=/; Domain=dev.example.com; Max-Age=\\d\\d; HttpOnly; Secure; SameSite=Strict",
 		cookies[0])
 	assert.NilError(t, err)
 	assert.Assert(t, matched)
 
 	assert.Equal(t, "signup=; Path=/; Domain=example.com; Max-Age=0; HttpOnly; Secure", cookies[1])
+}
+
+func TestCookieSecureFlagIsSetForTLS(t *testing.T) {
+	baseDomain := "example.com"
+	req := &http.Request{
+		Host:   fmt.Sprintf("dev.%s", baseDomain),
+		Header: make(http.Header),
+		TLS:    &tls.ConnectionState{}, // if this isnt nil the request is considered secure
+	}
+
+	resp := httptest.NewRecorder()
+	setCookie(
+		req,
+		resp,
+		cookieConfig{
+			Name:  "tls-cookie-test",
+			Value: "1234",
+		})
+	assert.Equal(t, len(resp.Header()["Set-Cookie"]), 1)
+	cookies := resp.Header()["Set-Cookie"]
+
+	assert.Equal(t, "tls-cookie-test=1234; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Strict", cookies[0])
 }
