45e0f3f24098a4c20c44844a8746cd313b226001
Daniel Nephin <dnephin@gmail.com>
2023-01-15 12:56:46 -0500

    Remove gin from scim access functions

--- internal/access/provider.go
+++ internal/access/provider.go
@@ -3,43 +3,41 @@ package access
 import (
 	"fmt"
 
-	"github.com/gin-gonic/gin"
-
 	"github.com/infrahq/infra/internal"
 	"github.com/infrahq/infra/internal/server/data"
 	"github.com/infrahq/infra/internal/server/models"
 	"github.com/infrahq/infra/uid"
 )
 
-func CreateProvider(c *gin.Context, provider *models.Provider) error {
-	db, err := RequireInfraRole(c, models.InfraAdminRole)
+func CreateProvider(rCtx RequestContext, provider *models.Provider) error {
+	err := IsAuthorized(rCtx, models.InfraAdminRole)
 	if err != nil {
 		return HandleAuthErr(err, "provider", "create", models.InfraAdminRole)
 	}
 
-	return data.CreateProvider(db, provider)
+	return data.CreateProvider(rCtx.DBTxn, provider)
 }
 
-func SaveProvider(c *gin.Context, provider *models.Provider) error {
-	db, err := RequireInfraRole(c, models.InfraAdminRole)
+func SaveProvider(rCtx RequestContext, provider *models.Provider) error {
+	err := IsAuthorized(rCtx, models.InfraAdminRole)
 	if err != nil {
 		return HandleAuthErr(err, "provider", "update", models.InfraAdminRole)
 	}
-	if data.InfraProvider(db).ID == provider.ID {
+	if data.InfraProvider(rCtx.DBTxn).ID == provider.ID {
 		return fmt.Errorf("%w: the infra provider can not be modified", internal.ErrBadRequest)
 	}
 
-	return data.UpdateProvider(db, provider)
+	return data.UpdateProvider(rCtx.DBTxn, provider)
 }
 
-func DeleteProvider(c *gin.Context, id uid.ID) error {
-	db, err := RequireInfraRole(c, models.InfraAdminRole)
+func DeleteProvider(rCtx RequestContext, id uid.ID) error {
+	err := IsAuthorized(rCtx, models.InfraAdminRole)
 	if err != nil {
 		return HandleAuthErr(err, "provider", "delete", models.InfraAdminRole)
 	}
-	if data.InfraProvider(db).ID == id {
+	if data.InfraProvider(rCtx.DBTxn).ID == id {
 		return fmt.Errorf("%w: the infra provider can not be deleted", internal.ErrBadRequest)
 	}
 
-	return data.DeleteProviders(db, data.DeleteProvidersOptions{ByID: id})
+	return data.DeleteProviders(rCtx.DBTxn, data.DeleteProvidersOptions{ByID: id})
 }
--- internal/access/scim.go
+++ internal/access/scim.go
@@ -4,68 +4,62 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/gin-gonic/gin"
-
 	"github.com/infrahq/infra/internal"
 	"github.com/infrahq/infra/internal/server/data"
 	"github.com/infrahq/infra/internal/server/models"
 	"github.com/infrahq/infra/uid"
 )
 
-func GetProviderUser(c *gin.Context, id uid.ID) (*models.ProviderUser, error) {
+func GetProviderUser(rCtx RequestContext, id uid.ID) (*models.ProviderUser, error) {
 	// IssuedFor will match no providers if called with a regular access key. When called with
 	// a SCIM access key it will be the provider ID. This effectively restricts this endpoint to
 	// only SCIM access keys.
-	ctx := GetRequestContext(c)
-	if err := checkKeyIdentityProvider(ctx); err != nil {
+	if err := checkKeyIdentityProvider(rCtx); err != nil {
 		return nil, err
 	}
-	user, err := data.GetProviderUser(ctx.DBTxn, ctx.Authenticated.AccessKey.IssuedFor, id)
+	user, err := data.GetProviderUser(rCtx.DBTxn, rCtx.Authenticated.AccessKey.IssuedFor, id)
 	if err != nil {
 		return nil, fmt.Errorf("get provider users: %w", err)
 	}
 	return user, nil
 }
 
-func ListProviderUsers(c *gin.Context, p *data.SCIMParameters) ([]models.ProviderUser, error) {
-	ctx := GetRequestContext(c)
+func ListProviderUsers(rCtx RequestContext, p *data.SCIMParameters) ([]models.ProviderUser, error) {
 	// restricted to only SCIM access keys
-	if err := checkKeyIdentityProvider(ctx); err != nil {
+	if err := checkKeyIdentityProvider(rCtx); err != nil {
 		return []models.ProviderUser{}, err
 	}
 	opts := data.ListProviderUsersOptions{
-		ByProviderID:   ctx.Authenticated.AccessKey.IssuedFor,
+		ByProviderID:   rCtx.Authenticated.AccessKey.IssuedFor,
 		SCIMParameters: p,
 	}
-	users, err := data.ListProviderUsers(ctx.DBTxn, opts)
+	users, err := data.ListProviderUsers(rCtx.DBTxn, opts)
 	if err != nil {
 		return nil, fmt.Errorf("list provider users: %w", err)
 	}
 	return users, nil
 }
 
-func CreateProviderUser(c *gin.Context, u *models.ProviderUser) error {
-	ctx := GetRequestContext(c)
+func CreateProviderUser(rCtx RequestContext, u *models.ProviderUser) error {
 	// restricted to only SCIM access keys
-	if err := checkKeyIdentityProvider(ctx); err != nil {
+	if err := checkKeyIdentityProvider(rCtx); err != nil {
 		return err
 	}
-	u.ProviderID = ctx.Authenticated.AccessKey.IssuedFor
-	err := data.ProvisionProviderUser(ctx.DBTxn, u)
+	u.ProviderID = rCtx.Authenticated.AccessKey.IssuedFor
+	err := data.ProvisionProviderUser(rCtx.DBTxn, u)
 	if err != nil {
 		return fmt.Errorf("provision provider user: %w", err)
 	}
 	return nil
 }
 
-func UpdateProviderUser(c *gin.Context, u *models.ProviderUser) error {
-	ctx := GetRequestContext(c)
+func UpdateProviderUser(rCtx RequestContext, u *models.ProviderUser) error {
 	// restricted to only SCIM access keys
-	if err := checkKeyIdentityProvider(ctx); err != nil {
+	if err := checkKeyIdentityProvider(rCtx); err != nil {
 		return err
 	}
-	u.ProviderID = ctx.Authenticated.AccessKey.IssuedFor
-	err := data.UpdateProviderUser(ctx.DBTxn, u)
+	u.ProviderID = rCtx.Authenticated.AccessKey.IssuedFor
+	err := data.UpdateProviderUser(rCtx.DBTxn, u)
 	if err != nil {
 		if errors.Is(err, data.ErrSourceOfTruthConflict) {
 			return fmt.Errorf("%w: %s", internal.ErrBadRequest, err)
@@ -75,41 +69,39 @@ func UpdateProviderUser(c *gin.Context, u *models.ProviderUser) error {
 	return nil
 }
 
-func PatchProviderUser(c *gin.Context, u *models.ProviderUser) (*models.ProviderUser, error) {
-	ctx := GetRequestContext(c)
+func PatchProviderUser(rCtx RequestContext, u *models.ProviderUser) (*models.ProviderUser, error) {
 	// restricted to only SCIM access keys
-	if err := checkKeyIdentityProvider(ctx); err != nil {
+	if err := checkKeyIdentityProvider(rCtx); err != nil {
 		return nil, err
 	}
-	u.ProviderID = ctx.Authenticated.AccessKey.IssuedFor
-	updated, err := data.PatchProviderUserActiveStatus(ctx.DBTxn, u)
+	u.ProviderID = rCtx.Authenticated.AccessKey.IssuedFor
+	updated, err := data.PatchProviderUserActiveStatus(rCtx.DBTxn, u)
 	if err != nil {
 		return nil, fmt.Errorf("patch provider user: %w", err)
 	}
 	return updated, nil
 }
 
-func DeleteProviderUser(c *gin.Context, userID uid.ID) error {
-	ctx := GetRequestContext(c)
+func DeleteProviderUser(rCtx RequestContext, userID uid.ID) error {
 	// restricted to only SCIM access keys
-	if err := checkKeyIdentityProvider(ctx); err != nil {
+	if err := checkKeyIdentityProvider(rCtx); err != nil {
 		return err
 	}
-	providerID := ctx.Authenticated.AccessKey.IssuedFor
+	providerID := rCtx.Authenticated.AccessKey.IssuedFor
 	// delete the provider user, and if its the last reference to the user, remove their identity also
 	opts := data.DeleteIdentitiesOptions{
 		ByProviderID: providerID,
 		ByIDs:        []uid.ID{userID},
 	}
-	if err := data.DeleteIdentities(ctx.DBTxn, opts); err != nil {
+	if err := data.DeleteIdentities(rCtx.DBTxn, opts); err != nil {
 		return fmt.Errorf("delete provider user identity: %w", err)
 	}
 	return nil
 }
 
-func checkKeyIdentityProvider(ctx RequestContext) error {
-	_, err := data.GetProvider(ctx.DBTxn,
-		data.GetProviderOptions{ByID: ctx.Authenticated.AccessKey.IssuedFor})
+func checkKeyIdentityProvider(rCtx RequestContext) error {
+	_, err := data.GetProvider(rCtx.DBTxn,
+		data.GetProviderOptions{ByID: rCtx.Authenticated.AccessKey.IssuedFor})
 	if err != nil {
 		if errors.Is(err, internal.ErrNotFound) {
 			return internal.ErrUnauthorized
