15494d21b6efabdac38451c87ae40d87fabff297
Daniel Nephin <dnephin@infrahq.com>
2023-01-17 18:27:40 -0500

    Support multiple channel listens on a single connection

--- internal/server/data/notify.go
+++ internal/server/data/notify.go
@@ -65,11 +65,22 @@ func (l *Listener) Release(ctx context.Context) error {
 	return nil
 }
 
-type ListenForNotifyOptions struct {
-	OrgID                                 uid.ID
-	GrantsByDestination                   string
-	DestinationCredentialsByDestinationID uid.ID
-	DestinationCredentialsByID            uid.ID
+// ListenChannelDescriptor provides a channel name that ListenForNotify uses
+// to listen for notifications.
+type ListenChannelDescriptor interface {
+	// Channel returns the name of the channel to listen on. The channel
+	// name is limited by the size of a postgres identifier. It should
+	// be no more than 40 characters, because 23 characters are reserved
+	// for the schema name that we need to prepend.
+	//
+	// The channel name should use camel case, and follow the convention of
+	//
+	//   <channel type>.<encoded orgID>.<encoded entity ID>
+	//
+	// The encoded ids use internal/uid base58 encoding. Encoded IDs are up to
+	// 11 characters long, so the channel type should be no more than 16
+	// characters.
+	Channel() string
 }
 
 // ListenForNotify starts listening for notification on one or more
@@ -79,11 +90,7 @@ type ListenForNotifyOptions struct {
 //
 // If error is nil the caller must call Listener.Release to return the database
 // connection to the pool.
-func ListenForNotify(ctx context.Context, db *DB, opts ListenForNotifyOptions) (*Listener, error) {
-	if opts.OrgID == 0 {
-		return nil, fmt.Errorf("OrgID is required")
-	}
-
+func ListenForNotify(ctx context.Context, db *DB, descriptors ...ListenChannelDescriptor) (*Listener, error) {
 	sqlDB := db.SQLdb()
 	pgxConn, err := pgxstdlib.AcquireConn(sqlDB)
 	if err != nil {
@@ -92,39 +99,70 @@ func ListenForNotify(ctx context.Context, db *DB, opts ListenForNotifyOptions) (
 
 	listener := &Listener{sqlDB: sqlDB, pgxConn: pgxConn}
 
-	var channel string
-	switch {
-	case opts.GrantsByDestination != "":
-		channel = fmt.Sprintf("grants_%d", opts.OrgID)
-	case opts.DestinationCredentialsByDestinationID != 0:
-		channel = fmt.Sprintf("credreq_%s_%s", opts.OrgID.String(), opts.DestinationCredentialsByDestinationID.String())
-	case opts.DestinationCredentialsByID != 0:
-		channel = fmt.Sprintf("credans_%s_%s", opts.OrgID.String(), opts.DestinationCredentialsByID.String())
-	}
-
-	logging.Debugf("listing for notify on %s", channel)
-	_, err = pgxConn.Exec(ctx, "SELECT listen_on_chan($1)", channel)
-	if err != nil {
-		if err := pgxstdlib.ReleaseConn(sqlDB, pgxConn); err != nil {
-			logging.L.Warn().Err(err).Msgf("release pgx conn")
+	for _, descriptor := range descriptors {
+		_, err = pgxConn.Exec(ctx, "SELECT listen_on_chan($1)", descriptor.Channel())
+		if err != nil {
+			if err := pgxstdlib.ReleaseConn(sqlDB, pgxConn); err != nil {
+				logging.L.Warn().Err(err).Msgf("release pgx conn")
+			}
+			return nil, err
 		}
-		return nil, err
-	}
 
-	switch {
-	case opts.GrantsByDestination != "":
-		listener.isMatchingNotify = func(payload string) error {
-			var grant grantJSON
-			err := json.Unmarshal([]byte(payload), &grant)
-			if err != nil {
-				return err
-			}
-			destination, _, _ := strings.Cut(grant.Resource, ".")
-			if destination != opts.GrantsByDestination {
-				return errNotificationNoMatch
-			}
-			return nil
+		// FIXME: this won't work now that we support multiple channels
+		if matcher, ok := descriptor.(interface {
+			Match(payload string) error
+		}); ok {
+			listener.isMatchingNotify = matcher.Match
 		}
 	}
 	return listener, nil
+}
+
+type ListenChannelGrantsByDestination struct {
+	OrgID       uid.ID
+	Destination string
+}
+
+func (l ListenChannelGrantsByDestination) Channel() string {
+	return fmt.Sprintf("grants_%d", l.OrgID)
+}
+
+func (l ListenChannelGrantsByDestination) Match(payload string) error {
+	var grant grantJSON
+	err := json.Unmarshal([]byte(payload), &grant)
+	if err != nil {
+		return err
+	}
+	destination, _, _ := strings.Cut(grant.Resource, ".")
+	if destination != l.Destination {
+		return errNotificationNoMatch
+	}
+	return nil
+}
+
+type ListenChannelDestinationCredentialsByDestinationID struct {
+	OrgID         uid.ID
+	DestinationID uid.ID
+}
+
+func (l ListenChannelDestinationCredentialsByDestinationID) Channel() string {
+	return fmt.Sprintf("credreq_%v_%v", l.OrgID, l.DestinationID)
+}
+
+type ListenChannelDestinationCredentialsByID struct {
+	OrgID                    uid.ID
+	DestinationCredentialsID uid.ID
+}
+
+func (l ListenChannelDestinationCredentialsByID) Channel() string {
+	return fmt.Sprintf("credans_%v_%v", l.OrgID, l.DestinationCredentialsID)
+}
+
+type ListenChannelGroupMembership struct {
+	OrgID   uid.ID
+	GroupID uid.ID
+}
+
+func (l ListenChannelGroupMembership) Channel() string {
+	return fmt.Sprintf("groupMembers.%v.%v", l.OrgID, l.GroupID)
 }
