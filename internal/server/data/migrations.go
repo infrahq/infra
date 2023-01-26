916fb4beb9c7a6b33461f91a1ce1813cf3de2ba4
Bruce MacDonald <brucewmacdonald@gmail.com>
2023-01-26 11:03:04 -0800

    fix: allow group names with commas
    - refactor migration column drop and add

--- internal/server/data/migrations.go
+++ internal/server/data/migrations.go
@@ -1359,10 +1359,12 @@ func storeProviderUserGroupsArray() *migrator.Migration {
 			if err != nil {
 				return err
 			}
-			if _, err := tx.Exec(`ALTER TABLE provider_users DROP COLUMN IF EXISTS groups;`); err != nil {
-				return err
-			}
-			if _, err := tx.Exec(`ALTER TABLE provider_users ADD COLUMN IF NOT EXISTS groups jsonb;`); err != nil {
+			stmt := `
+				ALTER TABLE provider_users 
+				DROP COLUMN IF EXISTS groups
+				ADD COLUMN IF NOT EXISTS groups jsonb;
+			`
+			if _, err := tx.Exec(stmt); err != nil {
 				return err
 			}
 			for _, u := range users {
